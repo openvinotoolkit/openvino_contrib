05b8830fd1138a8a8e1969b23eb0074adb996827
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-02-15 11:55:57 +0300

    More tests on ARM plugin (#42)
    * Enabled more ARM tests
    
    * Commented failed tests

--- modules/arm_plugin/tests/functional/shared_tests_instances/behavior/core_threading_tests.cpp
+++ modules/arm_plugin/tests/functional/shared_tests_instances/behavior/core_threading_tests.cpp
@@ -19,17 +19,16 @@ const Params paramsStreams[] = {
 
 }  // namespace
 
-INSTANTIATE_TEST_CASE_P(DISABLED_ARM, CoreThreadingTests, testing::ValuesIn(params));
+INSTANTIATE_TEST_CASE_P(smoke_BehaviorTests, CoreThreadingTests, testing::ValuesIn(params));
 
-// TODO: uncomment after merging https://github.com/openvinotoolkit/openvino/pull/3297
-// INSTANTIATE_TEST_CASE_P(DISABLED_ARM, CoreThreadingTestsWithIterations,
-//     testing::Combine(testing::ValuesIn(params),
-//                      testing::Values(4),
-//                      testing::Values(50),
-//                      testing::Values(ModelClass::Default)));
-//
-// INSTANTIATE_TEST_CASE_P(DISABLED_ARM_Streams, CoreThreadingTestsWithIterations,
-//     testing::Combine(testing::ValuesIn(paramsStreams),
-//                      testing::Values(4),
-//                      testing::Values(10),
-//                      testing::Values(ModelClass::Default)));
+INSTANTIATE_TEST_CASE_P(DISABLED_smoke_BehaviorTests, CoreThreadingTestsWithIterations,
+    testing::Combine(testing::ValuesIn(params),
+                     testing::Values(4),
+                     testing::Values(50),
+                     testing::Values(ModelClass::Default)));
+
+INSTANTIATE_TEST_CASE_P(DISABLED_smoke_Streams_BehaviorTests, CoreThreadingTestsWithIterations,
+    testing::Combine(testing::ValuesIn(paramsStreams),
+                     testing::Values(4),
+                     testing::Values(10),
+                     testing::Values(ModelClass::Default)));
