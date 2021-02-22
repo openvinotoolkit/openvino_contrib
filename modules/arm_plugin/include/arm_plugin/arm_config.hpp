3cc4eac2bfbc51b385faaeacc2b61bcc8569cb39
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-02-22 11:32:37 +0300

    Removed dummy HARDWARE_CONVOLUTION metric

--- modules/arm_plugin/include/arm_plugin/arm_config.hpp
+++ modules/arm_plugin/include/arm_plugin/arm_config.hpp
@@ -16,24 +16,6 @@
 
 namespace InferenceEngine {
 
-namespace ArmMetrics {
-
-/**
- * @def ARM_METRIC_VALUE(name)
- * @brief Shortcut for defining Arm metric values
- */
-#define ARM_METRIC_VALUE(name) InferenceEngine::ArmMetrics::name
-#define DECLARE_ARM_METRIC_VALUE(name) static constexpr auto name = #name
-
-// ! [public_header:metrics]
-/**
- * @brief Defines whether current Arm device instance supports hardware blocks for fast convolution computations.
- */
-DECLARE_ARM_METRIC_VALUE(HARDWARE_CONVOLUTION);
-// ! [public_header:metrics]
-
-}  // namespace ArmMetrics
-
 namespace ArmConfigParams {
 
 /**
@@ -46,8 +28,6 @@ namespace ArmConfigParams {
 #define DECLARE_ARM_CONFIG_VALUE(name) DECLARE_CONFIG_VALUE(ARM_##name)
 
 DECLARE_ARM_CONFIG_KEY(THROUGHPUT_STREAMS);
-
-
 
 }  // namespace ArmConfigParams
 }  // namespace InferenceEngine
