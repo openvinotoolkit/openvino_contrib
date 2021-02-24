3eda6d5df1f9747226ad604738d02db41b718f61
Ivan Novoselov <ivan.novoselov@intel.com>
2021-02-24 16:01:52 +0300

    Wrapper back. Depth is const check. Neg axis support.

--- modules/arm_plugin/src/arm_converter/arm_converter_one_hot.cpp
+++ modules/arm_plugin/src/arm_converter/arm_converter_one_hot.cpp
@@ -5,17 +5,43 @@
 #include "arm_converter/arm_converter.hpp"
 #include <ngraph/runtime/reference/one_hot.hpp>
 
+template <typename INPUT_TYPE>
+void wrap_one_hot(const INPUT_TYPE* indices,
+             const ngraph::Shape& indices_shape,
+             char* out,
+             const size_t out_elem_size,
+             const size_t depth,
+             const int64_t one_hot_axis,
+             const char* on_value,
+             const char* off_value) {
+    ngraph::runtime::reference::one_hot<INPUT_TYPE>(indices,
+                                            indices_shape,
+                                            out,
+                                            out_elem_size,
+                                            depth,
+                                            one_hot_axis,
+                                            on_value,
+                                            off_value);
+}
+
 namespace ArmPlugin {
     template<> Converter::Conversion::Ptr Converter::Convert(const opset::OneHot& node) {
+        const auto& ind_shape = node.get_input_shape(0);
         const auto& out_shape = node.get_output_shape(0);
-        const std::int64_t axis = node.get_axis();
-        if (axis < 0 || axis >= out_shape.size())
-            THROW_IE_EXCEPTION << "Invalid axis value. Expected in [0, " << out_shape.size() <<
-                                "), got " << axis;
+        std::int64_t axis = node.get_axis();
+        const auto out_rank = out_shape.size();
+        if (axis < -out_rank || axis >= out_rank)
+            THROW_IE_EXCEPTION << "Invalid axis value. Expected in [" << -out_rank
+                               << ", " << out_rank-1 << "]. Got " << axis;
+        if (axis < 0)
+            axis += out_rank;
+
         const auto& depth_const = std::dynamic_pointer_cast<opset::Constant>(node.input_value(1).get_node_shared_ptr());
+        if (!depth_const)
+            THROW_IE_EXCEPTION << "Depth value must be constant.";
+
         const auto depth = depth_const -> cast_vector<int64_t>()[0];
 
-        const auto& ind_shape = node.get_input_shape(0);
         if (ngraph::shape_size(ind_shape) * depth != ngraph::shape_size(out_shape))
             THROW_IE_EXCEPTION << "Incompatible I/O shapes or wrong depth value.";
         if (depth != out_shape[axis])
@@ -34,11 +60,11 @@ namespace ArmPlugin {
         };
         ngraph::element::Type_t inputType = node.get_input_element_type(0);
         switch (inputType) {
-            case ngraph::element::Type_t::u8 : return make(ngraph::runtime::reference::one_hot<std::uint8_t>);
-            case ngraph::element::Type_t::i16 : return make(ngraph::runtime::reference::one_hot<std::int16_t>);
-            case ngraph::element::Type_t::u16 : return make(ngraph::runtime::reference::one_hot<std::uint16_t>);
-            case ngraph::element::Type_t::i32 : return make(ngraph::runtime::reference::one_hot<std::int32_t>);
-            case ngraph::element::Type_t::u32 : return make(ngraph::runtime::reference::one_hot<std::uint32_t>);
+            case ngraph::element::Type_t::u8 : return make(wrap_one_hot<std::uint8_t>);
+            case ngraph::element::Type_t::i16 : return make(wrap_one_hot<std::int16_t>);
+            case ngraph::element::Type_t::u16 : return make(wrap_one_hot<std::uint16_t>);
+            case ngraph::element::Type_t::i32 : return make(wrap_one_hot<std::int32_t>);
+            case ngraph::element::Type_t::u32 : return make(wrap_one_hot<std::uint32_t>);
             default : THROW_IE_EXCEPTION << "Unsupported input type: " << inputType; return {};
         }
     }
