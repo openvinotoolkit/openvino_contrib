b4d4965ff86c9758af1c6b681519898ee7672a50
Ivan Novoselov <ivan.novoselov@intel.com>
2021-02-25 11:21:05 +0300

    Depth as node input. Checks in the wrapper.

--- modules/arm_plugin/src/arm_converter/arm_converter_one_hot.cpp
+++ modules/arm_plugin/src/arm_converter/arm_converter_one_hot.cpp
@@ -9,16 +9,22 @@ template <typename InputType, typename OutputType>
 void wrap_one_hot(const InputType* indices,
                   const ngraph::Shape& indices_shape,
                   OutputType* out,
+                  const ngraph::Shape& out_shape,
                   const size_t out_elem_size,
-                  const std::int64_t depth,
+                  const InputType* depth,
                   const std::int64_t one_hot_axis,
                   const OutputType* on_value,
                   const OutputType* off_value) {
+    const size_t depth_val = static_cast<size_t>(depth[0]);
+    if (ngraph::shape_size(indices_shape) * depth_val != ngraph::shape_size(out_shape))
+        THROW_IE_EXCEPTION << "Incompatible I/O shapes or wrong depth value.";
+    if (depth_val != out_shape[one_hot_axis])
+        THROW_IE_EXCEPTION << "Incompatible depth and axis values.";
     ngraph::runtime::reference::one_hot<InputType>(indices,
                                                    indices_shape,
                                                    reinterpret_cast<char*>(out),
                                                    out_elem_size,
-                                                   static_cast<size_t>(depth),
+                                                   depth_val,
                                                    one_hot_axis,
                                                    reinterpret_cast<const char*>(on_value),
                                                    reinterpret_cast<const char*>(off_value));
@@ -35,25 +41,14 @@ namespace ArmPlugin {
                                << ", " << out_rank-1 << "]. Got " << axis;
         if (axis < 0)
             axis += out_rank;
-
-        const auto& depth_const = std::dynamic_pointer_cast<opset::Constant>(node.input_value(1).get_node_shared_ptr());
-        if (!depth_const)
-            THROW_IE_EXCEPTION << "Depth value must be constant.";
-
-        const auto depth = depth_const -> cast_vector<int64_t>()[0];
-
-        if (ngraph::shape_size(ind_shape) * depth != ngraph::shape_size(out_shape))
-            THROW_IE_EXCEPTION << "Incompatible I/O shapes or wrong depth value.";
-        if (depth != out_shape[axis])
-            THROW_IE_EXCEPTION << "Incompatible depth and axis values.";
-
         auto make = [&] (auto refFunction) {
             return MakeConversion(refFunction,
                                   node.input(0),
                                   ind_shape,
                                   node.output(0),
+                                  out_shape,
                                   node.get_output_element_type(0).size(),
-                                  depth,
+                                  node.input(1),
                                   axis,
                                   node.input(2),
                                   node.input(3));
