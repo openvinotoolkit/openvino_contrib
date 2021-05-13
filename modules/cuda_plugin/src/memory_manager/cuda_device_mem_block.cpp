0eb33b27308f63cdce181c41212cfa0926e5a0f1
vitaliy.mostovy <vitaliy.mostovy@altran.com>
2021-05-13 18:13:34 +0300

    Memory Manager tests
    Rename MemoryModel::deviceMemoryBlobSize() to deviceMemoryBlockSize

--- modules/cuda_plugin/src/memory_manager/cuda_device_mem_block.cpp
+++ modules/cuda_plugin/src/memory_manager/cuda_device_mem_block.cpp
@@ -14,7 +14,7 @@ namespace CUDAPlugin {
 DeviceMemBlock::DeviceMemBlock(MemoryModel::Ptr model)
   : model_{ model }, device_mem_ptr_{ nullptr }
 {
-  auto err = ::cudaMalloc(&device_mem_ptr_, model_->deviceMemoryBlobSize());
+  auto err = ::cudaMalloc(&device_mem_ptr_, model_->deviceMemoryBlockSize());
   if (err != cudaSuccess)
     THROW_IE_EXCEPTION
       << "::cudaMalloc() failed: "
@@ -35,7 +35,7 @@ DeviceMemBlock::~DeviceMemBlock() {
 
 void* DeviceMemBlock::deviceTensorPtr(MemoryModel::TensorID id) {
   ptrdiff_t offset = 0;
-  if (model_->offsetForTensor(id, offset))
+  if (device_mem_ptr_ && model_->offsetForTensor(id, offset))
     return reinterpret_cast<uint8_t*>(device_mem_ptr_) + offset;
   else
     return nullptr;
