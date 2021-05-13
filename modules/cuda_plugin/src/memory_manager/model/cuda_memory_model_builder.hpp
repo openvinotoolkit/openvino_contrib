0f370d3eb8d90eb7efb30efca82f433eca3e7798
vitaliy.mostovy <vitaliy.mostovy@altran.com>
2021-05-13 18:13:34 +0300

    Memory Manager tests
    Rename MemoryModel::deviceMemoryBlobSize() to deviceMemoryBlockSize

--- modules/cuda_plugin/src/memory_manager/model/cuda_memory_model_builder.hpp
+++ modules/cuda_plugin/src/memory_manager/model/cuda_memory_model_builder.hpp
@@ -22,14 +22,15 @@ public:
    *
    * @param [in] id Tensor identifier. Will be used to obtain device side
    * tensor pointer.
-   * @param [in] start Execution order index of first use. The data is
+   * @param [in] producerIndex Execution order index of first use. The data is
    * produced here.
-   * @param [in] finish The execution order index of last use. After that
+   * @param [in] lastConsumerIndex The execution order index of last use. After that
    * data will be released. -1 is a reserved value for "till to end".
    * @param [in] bsize Tensor memory size in bytes.
-   * @throws InferenceEngineException if tensor is already added.
+   * @throws InferenceEngineException if allocation size is zero or tensor
+   * with specified id is already added.
    */
-  void addAllocation(TensorID id, int start, int finish, size_t bsize);
+  void addAllocation(TensorID id, int producerIndex, int lastConsumerIndex, size_t bsize);
 
   /**
    * Creates and initializes MemoryModel object.
---
+++ modules/cuda_plugin/tests/unit/memory_manager/model/cuda_immutable_memory_model_builder_test.cpp
@@ -0,0 +1,93 @@
+// Copyright (C) 2018-2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <gtest/gtest.h>
+
+#include "memory_manager/model/cuda_immutable_memory_model_builder.hpp"
+#include "memory_manager/model/details/cuda_memory_utils.hpp"
+
+#include <details/ie_exception.hpp>
+
+
+TEST(ImmutableMemoryModelBuilder, BuildEmpty) {
+  using namespace CUDAPlugin;
+
+  ImmutableMemoryModelBuilder builder;
+  MemoryModel::Ptr model = builder.build();
+  ASSERT_EQ(model->deviceMemoryBlockSize(), 0);
+}
+
+TEST(ImmutableMemoryModelBuilder, Build) {
+  using namespace CUDAPlugin;
+
+  MemoryModel::TensorID id1 = 0;
+  size_t size1 = 1;
+  MemoryModel::TensorID id2 = 3;
+  size_t size2 = 256;
+  MemoryModel::TensorID id3 = 7;
+  size_t size3 = 784;
+  MemoryModel::TensorID id4 = 9;
+  size_t size4 = 1396;
+
+  ImmutableMemoryModelBuilder builder;
+  builder.addAllocation(id1, size1);
+  builder.addAllocation(id2, size2);
+  builder.addAllocation(id3, size3);
+  builder.addAllocation(id4, size4);
+  MemoryModel::Ptr model = builder.build();
+
+  ptrdiff_t offset1 = -1;
+  ASSERT_TRUE(model->offsetForTensor(id1, offset1));
+  ASSERT_EQ(offset1, 0);
+
+  ptrdiff_t offset2 = -1;
+  ASSERT_TRUE(model->offsetForTensor(id2, offset2));
+  ASSERT_EQ(offset2, offset1 + applyAllignment(size1));
+
+  ptrdiff_t offset3 = -1;
+  ASSERT_TRUE(model->offsetForTensor(id3, offset3));
+  ASSERT_EQ(offset3, offset2 + applyAllignment(size2));
+
+  ptrdiff_t offset4 = -1;
+  ASSERT_TRUE(model->offsetForTensor(id4, offset4));
+  ASSERT_EQ(offset4, offset3 + applyAllignment(size3));
+
+  ASSERT_EQ(model->deviceMemoryBlockSize(), offset4 + applyAllignment(size4));
+}
+
+TEST(ImmutableMemoryModelBuilder, HandleDuplicateAllocation) {
+  using namespace CUDAPlugin;
+
+  ImmutableMemoryModelBuilder builder;
+
+  MemoryModel::TensorID duplicate_tensor_id = 1;
+  size_t size1 = 128;
+  size_t size2 = 256;
+
+  builder.addAllocation(duplicate_tensor_id, size1);
+
+  #ifdef NDEBUG
+    ASSERT_THROW(builder.addAllocation(duplicate_tensor_id, size1), InferenceEngine::details::InferenceEngineException);
+    ASSERT_THROW(builder.addAllocation(duplicate_tensor_id, size2), InferenceEngine::details::InferenceEngineException);
+  #else
+    testing::FLAGS_gtest_death_test_style = "threadsafe";
+    ASSERT_DEATH(builder.addAllocation(duplicate_tensor_id, size1), "Assertion");
+    ASSERT_DEATH(builder.addAllocation(duplicate_tensor_id, size2), "Assertion");
+  #endif
+}
+
+TEST(ImmutableMemoryModelBuilder, HandleZeroAllocationSize) {
+  using namespace CUDAPlugin;
+
+  ImmutableMemoryModelBuilder builder;
+
+  MemoryModel::TensorID tensor_id = 1;
+
+  #ifdef NDEBUG
+    ASSERT_THROW(builder.addAllocation(tensor_id, 0), InferenceEngine::details::InferenceEngineException);
+  #else
+    testing::FLAGS_gtest_death_test_style = "threadsafe";
+    ASSERT_DEATH(builder.addAllocation(tensor_id, 0), "Assertion");
+  #endif
+}
