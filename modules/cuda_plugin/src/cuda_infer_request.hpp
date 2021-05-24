ed67688fd3b8d5d327c310a971532911f849dc34
Yaroslav Torzuk <yaroslav.torzuk2@altran.com>
2021-05-24 13:44:10 +0300

    Adds cutensor in cmake configuration
    Adds UniqueBase template specialization for non-pointer types
    
    Adds throwIfError and logIfError specializations for cutensorStatus_t
    
    Adds a wrapper for cuTENSOR handle
    
    Integrates CuTensorHandle into ThreadContext
    
    Adds transpose op implementation
    
    Adds single layer tests which cover Transpose op functionality
    
    Fixes compilation error in TransposeOp
    
    Removes unused tensors creation
    
    Adds fp16 inputs support
    
    Refactoring of CudaInferRequest::allocateBlobs() method
    
    Transforms static function copyBlob to CudaInferRequest::convertPrecision()
    
    Makes InferenceRequestContext using network inputs/outputs instead of user ones
    
    Adds CudaInferRequest::convertPrecision template specializations for __half <-> float conversion
    
    Adds conversion of fp32 user blobs to fp16 network blobs
    
    Enables tests with fp16 network precision which cover Transpose op

--- modules/cuda_plugin/src/cuda_infer_request.hpp
+++ modules/cuda_plugin/src/cuda_infer_request.hpp
@@ -57,8 +57,25 @@ public:
     void Cancel() override;
 
 private:
-    void allocateDeviceBuffers();
-    void allocateBlobs();
+    /**
+     * Allocates blob with given shape, type and layout
+     */
+    static InferenceEngine::Blob::Ptr allocateBlob(
+            const std::vector<std::size_t>& shape, InferenceEngine::Precision precision,
+            InferenceEngine::Layout layout);
+    /**
+     * ngraph::element::Type_t to InferenceEngine::Precision::ePrecision conversion helper
+     */
+    static InferenceEngine::Precision::ePrecision convertType(ngraph::element::Type_t);
+    /**
+     * Converts blob data from src blob type to dst blob type. Writes result into dst
+     */
+    static void convertPrecision(const InferenceEngine::Blob::Ptr& src, const InferenceEngine::Blob::Ptr& dst);
+    /**
+     * Converts blob data from SrcT blob type to DstT blob type. Writes result into dst.
+     */
+    template<typename SrcT, typename DstT>
+    static void convertPrecision(const InferenceEngine::Blob::Ptr& src, const InferenceEngine::Blob::Ptr& dst);
 
     enum {
         Preprocess,
@@ -73,10 +90,14 @@ private:
     // for performance counters
     std::array<std::chrono::duration<float, std::micro>, numOfStages>   _durations;
 
+    /**
+     * InferRequestInternal::execDataPreprocessing() doesn't support conversion from fp32 to fp16.
+     * fp16NetworkInputBlobs_ holds fp16 network blobs while InferRequestInternal::execDataPreprocessing()
+     * performs preprocessing on fp32 blobs.
+     */
+    InferenceEngine::BlobMap                                fp16NetworkInputBlobs_;
     InferenceEngine::BlobMap                                _networkOutputBlobs;
 
-    std::vector<std::shared_ptr<ngraph::runtime::Tensor>>   _inputTensors;
-    std::vector<std::shared_ptr<ngraph::runtime::Tensor>>   _outputTensors;
     std::optional<MemoryManagerPool::Proxy>                 memory_manager_proxy_;
     CancellationToken                                       cancellation_token_;
 };
---
+++ modules/cuda_plugin/src/ops/transpose.hpp
@@ -0,0 +1,84 @@
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <cuda_operation_base.hpp>
+#include <optional>
+#include <unordered_map>
+#include <vector>
+#include <ngraph/node.hpp>
+#include <cuda.h>
+#include <cuda_bf16.h>
+#include <cuda_fp16.h>
+
+namespace CUDAPlugin {
+
+class TransposeOp : public OperationBase {
+public:
+    TransposeOp(const std::shared_ptr<ngraph::Node>& node,
+             std::vector<unsigned>&& inputIds,
+             std::vector<unsigned>&& outputIds);
+    void Execute(const InferenceRequestContext& context,
+                 Inputs inputTensors,
+                 Outputs outputTensors) override;
+
+private:
+    using ExtentsMap = std::unordered_map<int, std::int64_t>;
+    union AlphaValue {
+        __half fp16;
+        __nv_bfloat16 bf16;
+        float fp32;
+        double fp64;
+        std::int8_t i8;
+        std::uint8_t u8;
+        std::int16_t i16;
+        std::uint16_t u16;
+        std::int32_t i32;
+        std::uint32_t u32;
+        std::int64_t i64;
+        std::uint64_t u64;
+    };
+
+    static std::vector<std::int64_t> extractInputExtents(const ngraph::Node& node);
+
+    static std::vector<std::int64_t> extractOutputExtents(const ngraph::Node& node);
+
+    static std::vector<int> extractInputMode(std::size_t numDims);
+
+    static std::vector<std::int64_t> extractInputStrides(const ngraph::Node& node);
+
+    static std::vector<std::int64_t> extractOutputStrides(const ngraph::Node& node);
+
+    static ExtentsMap extractExtents(const std::vector<std::int64_t>& input_extents);
+
+    static bool isPermutationTensorSpecified(const ngraph::Node& node);
+
+    static std::optional<std::vector<int>> tryToExtractPermutation(const ngraph::Node& node);
+
+    std::vector<int> permutation(const InferenceRequestContext& context, Inputs inputTensors) const;
+
+    ngraph::element::Type_t extractPermutationElementsType(const ngraph::Node& node);
+
+    static AlphaValue makeAlpha(cudaDataType_t dt);
+
+    template<typename T>
+    static std::vector<int> downloadPermutationVector(const InferenceRequestContext& context,
+            InferenceEngine::gpu::DevicePointer<const void*>, unsigned numDims);
+
+private:
+    std::vector<std::int64_t> inputExtents_;
+    std::size_t dimsNumber_;
+    std::vector<std::int64_t> outputExtents_;
+    std::vector<std::int64_t> inputStrides_;
+    std::vector<std::int64_t> outputStrides_;
+    std::vector<int> inputMode_;
+    std::optional<std::vector<int>> outputMode_;
+    ExtentsMap extents_;
+    cudaDataType_t inputElementsType_;
+    ngraph::element::Type_t permutationElementsType_;
+    AlphaValue alpha_;
+};
+
+} // namespace CUDAPlugin
