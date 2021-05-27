79b3fd362063d9ecf7158742a9333a6e0dc0324b
Volodymyr Havrylov <volodymyr.havrylov@altran.com>
2021-05-27 14:08:56 +0300

    Adds pooling decriptor
    * Also extracts cuDNN type conversion function to a common header
    
    Adds base pooling operation impl.
    
    Adds MaxPool op
    
    Adds AvgPool op
    
    Adds pooling simple unit tests for 1d, 2d, 3d pooling
    
    Adds single-layer pooling tests
    
    Pooling ops construction refactoring
    
    Renames BasePooling to PoolingImpl
    
    Adds skeleton of the GraphTransformer
    
    Integrates GraphTransformer to the Plugin

---
+++ modules/cuda_plugin/src/cuda_graph_transformer.hpp
@@ -0,0 +1,31 @@
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <cpp_interfaces/impl/ie_plugin_internal.hpp>
+
+namespace CUDAPlugin {
+
+class GraphTransformer {
+ public:
+  /**
+   * @brief Transform takes an ngraph::Function and applies all the necessary
+   *        graph transofrmations to achieve the maximum optimization of the
+   *        model for execution on a CUDA device. The transformations may
+   *        include CUDA-specific op fusions and some common OpenVino
+   *        transformations as well.
+   * @param function a valid shared ptr to a model, represented as an
+   *        ngraph::Function instance.
+   * @param config a string-string map of configuration for loading an
+   * executable network (e.g. a model); this config influences on what exact
+   *        transformations are being applied to the original graph.
+   * @return an ngraph::Function containing only the CUDA-optimized operations.
+   */
+  std::shared_ptr<ngraph::Function> transform(
+      const std::shared_ptr<const ngraph::Function>& function,
+      const std::map<std::string, std::string>& config) const;
+};
+
+}  // namespace CUDAPlugin
--- modules/cuda_plugin/src/ops/relu.cpp
+++ modules/cuda_plugin/src/ops/relu.cpp
@@ -6,29 +6,7 @@
 #include <ngraph/node.hpp>
 
 namespace CUDAPlugin {
-namespace {
-constexpr cudnnDataType_t toDataType(const ngraph::element::Type& type) {
-  using ngraph::element::Type_t;
-  switch (static_cast<Type_t>(type)) {
-    case Type_t::bf16:
-      return CUDNN_DATA_BFLOAT16;
-    case Type_t::f16:
-      return CUDNN_DATA_HALF;
-    case Type_t::f32:
-      return CUDNN_DATA_FLOAT;
-    case Type_t::f64:
-      return CUDNN_DATA_DOUBLE;
-    case Type_t::i8:
-      return CUDNN_DATA_INT8;
-    case Type_t::i32:
-      return CUDNN_DATA_INT32;
-    case Type_t::i64:
-      return CUDNN_DATA_INT64;
-    default:
-      CUDA::throwIEException("unsupported ngraph element type " +
-                             type.c_type_string());
-  }
-}
+namespace  {
 
 std::array<int, 5> toArray(const ngraph::Shape& shape) {
   std::array<int, 5> a{1, 1, 1, 1, 1};
@@ -48,7 +26,7 @@ CUDA::DnnTensorDescriptor desc(
   strides.back() = 1;
   for (int i = dims.size() - 1; i > 0; i--)
     strides[i - 1] = strides[i] * dims[i];
-  return {toDataType(type), dims.size(), dims.data(), strides.data()};
+  return {CUDA::toDataType(type), dims.size(), dims.data(), strides.data()};
 }
 
 CUDA::DnnTensorDescriptor inputDesc(const ngraph::Node& node, int n) {
