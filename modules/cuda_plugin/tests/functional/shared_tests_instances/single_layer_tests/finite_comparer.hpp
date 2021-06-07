4eca3fb5569d9c76fd11a8924f05b99fb8927739
Denis Kotov <denys.kotov@altran.com>
2021-06-07 16:45:44 +0300

    Created FiniteComparer for excluding infinite values from comparasion
    Created test class based on FiniteComparer<MatMulTest> and enabled testing FP16 precision
    
    Update FP32 tests for MatMul for proper precision

---
+++ modules/cuda_plugin/tests/functional/shared_tests_instances/single_layer_tests/finite_comparer.hpp
@@ -0,0 +1,97 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include "shared_test_classes/base/layer_test_utils.hpp"
+
+namespace LayerTestsDefinitions {
+
+template <typename BaseLayerTest>
+class FiniteComparer : public BaseLayerTest {
+    static_assert(std::is_base_of<LayerTestsUtils::LayerTestsCommon, BaseLayerTest>::value,
+                  "BaseLayerTest should inherit from LayerTestsUtils::LayerTestsCommon");
+
+    template<class T>
+    static void Compare(const T *expected, const T *actual, std::size_t size, T threshold) {
+        for (std::size_t i = 0; i < size; ++i) {
+            const auto &ref = expected[i];
+            const auto &res = actual[i];
+            if (std::isinf(ref) && std::isinf(res)) {
+                continue;
+            }
+            const auto absoluteDifference = CommonTestUtils::ie_abs(res - ref);
+            if (absoluteDifference <= threshold) {
+                continue;
+            }
+
+            const auto max = std::max(CommonTestUtils::ie_abs(res), CommonTestUtils::ie_abs(ref));
+            float diff = static_cast<float>(absoluteDifference) / static_cast<float>(max);
+            ASSERT_TRUE(max != 0 && (diff <= static_cast<float>(threshold)))
+                                << "Relative comparison of values expected: " << ref << " and actual: " << res
+                                << " at index " << i << " with threshold " << threshold
+                                << " failed";
+        }
+    }
+
+    void Compare(const std::vector<std::uint8_t> &expected, const InferenceEngine::Blob::Ptr &actual) override {
+        ASSERT_EQ(expected.size(), actual->byteSize());
+        const auto &expectedBuffer = expected.data();
+
+        auto memory = InferenceEngine::as<InferenceEngine::MemoryBlob>(actual);
+        IE_ASSERT(memory);
+        const auto lockedMemory = memory->wmap();
+        const auto actualBuffer = lockedMemory.as<const std::uint8_t *>();
+
+        const auto &precision = actual->getTensorDesc().getPrecision();
+        const auto &size = actual->size();
+        switch (precision) {
+            case InferenceEngine::Precision::FP32:
+                Compare<float>(reinterpret_cast<const float *>(expectedBuffer),
+                               reinterpret_cast<const float *>(actualBuffer), size, this->threshold);
+                break;
+            case InferenceEngine::Precision::I32:
+                Compare<int32_t>(reinterpret_cast<const int32_t *>(expectedBuffer),
+                                 reinterpret_cast<const int32_t *>(actualBuffer), size, 0);
+                break;
+            case InferenceEngine::Precision::I64:
+                Compare<int64_t>(reinterpret_cast<const int64_t *>(expectedBuffer),
+                                 reinterpret_cast<const int64_t *>(actualBuffer), size, 0);
+                break;
+            case InferenceEngine::Precision::I8:
+                Compare<int8_t>(reinterpret_cast<const int8_t *>(expectedBuffer),
+                                reinterpret_cast<const int8_t *>(actualBuffer), size, 0);
+                break;
+            case InferenceEngine::Precision::U16:
+                Compare<uint16_t>(reinterpret_cast<const uint16_t *>(expectedBuffer),
+                                  reinterpret_cast<const uint16_t *>(actualBuffer), size, 0);
+                break;
+            case InferenceEngine::Precision::I16:
+                Compare<int16_t>(reinterpret_cast<const int16_t *>(expectedBuffer),
+                                 reinterpret_cast<const int16_t *>(actualBuffer), size, 0);
+                break;
+            case InferenceEngine::Precision::BOOL:
+            case InferenceEngine::Precision::U8:
+                Compare<uint8_t>(reinterpret_cast<const uint8_t *>(expectedBuffer),
+                                 reinterpret_cast<const uint8_t *>(actualBuffer), size, 0);
+                break;
+            case InferenceEngine::Precision::U64:
+                Compare<uint64_t>(reinterpret_cast<const uint64_t *>(expectedBuffer),
+                                  reinterpret_cast<const uint64_t *>(actualBuffer), size, 0);
+                break;
+            case InferenceEngine::Precision::BF16:
+                Compare(reinterpret_cast<const ngraph::bfloat16 *>(expectedBuffer),
+                        reinterpret_cast<const ngraph::bfloat16 *>(actualBuffer), size, ngraph::bfloat16(this->threshold));
+                break;
+            case InferenceEngine::Precision::FP16:
+                Compare(reinterpret_cast<const ngraph::float16 *>(expectedBuffer),
+                        reinterpret_cast<const ngraph::float16 *>(actualBuffer), size, ngraph::float16(this->threshold));
+                break;
+            default:
+                FAIL() << "Comparator for " << precision << " precision isn't supported";
+        }
+    }
+};
+
+}
