30bf8db458b96ca0803532d74de4894a52b058bc
Andrii Pavliuk <andrii.andrii@altran.com>
2021-06-08 19:43:25 +0300

    Add OpTensorBaseOp base class for Add, Mul etc.
    Add Add operation inherited from OpTensorBaseOp
    
    Add tests for Add operation

---
+++ modules/cuda_plugin/src/ops/add.hpp
@@ -0,0 +1,18 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include "cudnn_tensor_op_base.hpp"
+
+namespace CUDAPlugin {
+
+class AddOp : public CuDnnTensorOpBase {
+  public:
+    AddOp(const std::shared_ptr<ngraph::Node>& node,
+          IndexCollection&& inputIds, IndexCollection&& outputIds);
+    ~AddOp() override = default;
+};
+
+}  // namespace CUDAPlugin
---
+++ modules/cuda_plugin/src/ops/cudnn_tensor_op_base.cpp
@@ -0,0 +1,116 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+#include "cudnn_tensor_op_base.hpp"
+
+#include <cuda_operation_registry.hpp>
+#include <ngraph/op/util/attr_types.hpp>
+
+namespace CUDAPlugin {
+namespace {
+
+template <typename T, std::size_t N>
+std::array<T, N> toArray(const ngraph::Shape& shape) {
+  std::array<T, N> a;
+  a.fill(static_cast<T>(1));
+  if (shape.empty()) return a;
+  std::copy(shape.rbegin(), shape.rend(), a.rbegin());
+  return a;
+}
+
+using ShapeArray = std::array<int, CuDnnTensorOpBase::max_supported_shape_size>;
+
+CUDA::DnnTensorDescriptor desc(
+    const cudnnDataType_t type, ShapeArray& dims) {
+  ShapeArray strides;
+  strides.back() = 1;
+  for (int i = dims.size() - 1; i > 0; i--)
+    strides[i - 1] = strides[i] * dims[i];
+  return { type, dims.size(), dims.data(), strides.data() };
+}
+}  // namespace
+
+CuDnnTensorOpBase::CuDnnTensorOpBase(const std::shared_ptr<ngraph::Node>& node,
+             IndexCollection&& inputIds, IndexCollection&& outputIds,
+             const cudnnOpTensorOp_t& opType, const cudnnNanPropagation_t& nanPropogationType)
+    : OperationBase{node, move(inputIds), move(outputIds)},
+      in0(*node, IoParams::Type::INPUT, 0),
+      in1(*node, IoParams::Type::INPUT, 1),
+      out(*node, IoParams::Type::OUTPUT, 0),
+      // According to https://docs.nvidia.com/deeplearning/cudnn/api/index.html#cudnnOpTensor
+      // opTensorCompType for cudnnOpTensor should always be FLOAT unless inputs and outputs are
+      // double which is not supported by CudaPlugin
+      op_desc_(opType, cudnnDataType_t::CUDNN_DATA_FLOAT, nanPropogationType) {
+  Expects(node->get_input_size() == 2);
+  Expects(node->get_output_size() == 1);
+  const auto& in_partial_shape0 = node->get_input_partial_shape(0);
+  const auto& in_partial_shape1 = node->get_input_partial_shape(1);
+  const auto& out_partial_shape = node->get_output_partial_shape(0);
+  if (in0.shape_.size() > max_supported_shape_size
+      || in1.shape_.size() > max_supported_shape_size) {
+    THROW_IE_EXCEPTION << "Currently max supported shape size for CuDnnTensorOpBase operation is: " <<
+      max_supported_shape_size << ' ' << in_partial_shape0 << ' ' << in_partial_shape1;
+  }
+  if (out.shape_ != in0.shape_ && out.shape_ != in1.shape_) {
+    THROW_IE_EXCEPTION << "Currently at least one of the input shapes: " <<
+        in_partial_shape0 << ' ' << in_partial_shape1 << " of CuDnnTensorOpBase operation should be"
+        "equal to the output shape: " << out_partial_shape;
+  }
+  const auto size = in0.array_.size();
+  Expects(in1.array_.size() == size);
+  Expects(out.array_.size() == size);
+  scaling_params_.set(out.type_, 1.0, 0.0);
+  bool has_0_broadcasts = false;
+  bool has_1_broadcasts = false;
+  for (int i = 0; i < size; ++i) {
+    if (in0.array_[i] != in1.array_[i]) {
+      if (in0.array_[i] == 1) {
+        has_0_broadcasts = true;
+      } else if (in1.array_[i] == 1) {
+        has_1_broadcasts = true;
+      } else {
+        THROW_IE_EXCEPTION << "Unsupported shapes for CuDnnTensorOpBase operation: " <<
+            in_partial_shape0 << ' ' << in_partial_shape1;
+      }
+    }
+  }
+  bias_index_ = 0;
+  dest_index_ = 1;
+  if (has_0_broadcasts || has_1_broadcasts) {
+    auto broadcast_spec = node->get_autob();
+    if (!(broadcast_spec == ngraph::op::AutoBroadcastSpec::NUMPY)) {
+      THROW_IE_EXCEPTION << "Unsupported broadcast type for CuDnnTensorOpBase operation: " <<
+          broadcast_spec.m_type;
+    }
+    if (has_0_broadcasts && has_1_broadcasts) {
+      THROW_IE_EXCEPTION << "Currently CuDnnTensorOpBase operation supports broadcasting only in one "
+      "of two input shapes: " << in_partial_shape0 << ' ' << in_partial_shape1;
+    }
+    bias_index_ = has_0_broadcasts ? 0 : 1;
+    dest_index_ = has_0_broadcasts ? 1 : 0;
+  }
+}
+
+void CuDnnTensorOpBase::Execute(const InferenceRequestContext& context, Inputs inputTensors,
+                    Outputs outputTensors) {
+  Expects(inputTensors.size() == 2);
+  Expects(outputTensors.size() == 1);
+  const auto& bias_input = bias_index_ == 0 ? in0 : in1;
+  const auto& dest_input = bias_index_ == 0 ? in1 : in0;
+  const auto alpha = scaling_params_.alpha();
+  const auto beta = scaling_params_.beta();
+  context.getThreadContext().dnnHandle().opTensor(op_desc_,
+      &alpha, dest_input.desc_, inputTensors[dest_index_].get(),
+      &alpha, bias_input.desc_, inputTensors[bias_index_].get(),
+      &beta, out.desc_, outputTensors[0].get());
+}
+
+CuDnnTensorOpBase::IoParams::IoParams(const ngraph::Node& node, const Type& io_type, int index)
+    : type_(CUDA::toDataType(io_type == Type::INPUT ? node.get_input_element_type(index)
+                                                    : node.get_output_element_type(index))),
+      shape_(io_type == Type::INPUT ? node.get_input_shape(index)
+                                    : node.get_output_shape(index)),
+      array_(toArray<int, max_supported_shape_size>(shape_)),
+      desc_(desc(type_, array_)) {}
+
+}  // namespace CUDAPlugin
