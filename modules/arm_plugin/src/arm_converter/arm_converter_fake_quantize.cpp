be98dc65d1150e6c2d76a9225c5f0852285ab4db
Ilya Lavrenov <ilya.lavrenov@intel.com>
2021-06-21 20:12:44 +0300

    Fixed compilation with gcc-5

--- modules/arm_plugin/src/arm_converter/arm_converter_fake_quantize.cpp
+++ modules/arm_plugin/src/arm_converter/arm_converter_fake_quantize.cpp
@@ -11,12 +11,12 @@
 namespace ArmPlugin {
 template<> Converter::Conversion::Ptr Converter::Convert(const opset::FakeQuantize& node) {
     auto make = [&] (auto refFunction) {
-        return MakeConversion(refFunction,
-                              node.input(0), node.input(1), node.input(2), node.input(3), node.input(4),
-                              node.output(0),
-                              node.input(0).get_shape(),
-                              node.input(1).get_shape(), node.input(2).get_shape(), node.input(3).get_shape(), node.input(4).get_shape(),
-                              node.get_levels());
+        return this->MakeConversion(refFunction,
+                                    node.input(0), node.input(1), node.input(2), node.input(3), node.input(4),
+                                    node.output(0),
+                                    node.input(0).get_shape(),
+                                    node.input(1).get_shape(), node.input(2).get_shape(), node.input(3).get_shape(), node.input(4).get_shape(),
+                                    node.get_levels());
     };
     switch (node.input(0).get_element_type()) {
         case ngraph::element::Type_t::f16 : return make(ngraph::runtime::reference::fake_quantize<ngraph::float16>);
