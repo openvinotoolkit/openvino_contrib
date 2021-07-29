351fb3cb672a3886bb90340ee965161c7094e478
Aleksandr Voron <aleksandr.voron@intel.com>
2021-07-29 23:42:10 +0300

    Update armplg_build.sh

--- modules/arm_plugin/armplg_build.sh
+++ modules/arm_plugin/armplg_build.sh
@@ -92,8 +92,9 @@ PYTHONVER=`ls /usr/include | grep "python3[^m]*$"` && \
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_LIST=imgcodecs,videoio,highgui,gapi,python3 \
       -DBUILD_opencv_python2=OFF -DBUILD_opencv_python3=ON -DOPENCV_SKIP_PYTHON_LOADER=ON \
       -DPYTHON3_LIMITED_API=ON -DPYTHON3_PACKAGES_PATH=$STAGING_DIR/opencv/python \
-      -DPYTHON3_INCLUDE_PATH=/usr/include/${PYTHONVER}m \
+      -DPYTHON3_INCLUDE_PATH=/usr/include/${PYTHONVER} \
       -DPYTHON3_LIBRARIES=/usr/lib/$ARCH_NAME/lib${PYTHONVER}m.so \
+      -DPYTHON3_NUMPY_INCLUDE_DIRS=/usr/lib/python3/dist-packages/numpy/core/include \
       -D CMAKE_USE_RELATIVE_PATHS=ON \
       -D CMAKE_SKIP_INSTALL_RPATH=ON \
       -D OPENCV_SKIP_PKGCONFIG_GENERATION=ON \
@@ -109,15 +110,13 @@ cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_LIST=imgcodecs,videoio,highgui,gapi
       -D OPENCV_OTHER_INSTALL_PATH=etc \
       -D OPENCV_LICENSES_INSTALL_PATH=etc/licenses \
       -D ENABLE_CXX11=ON \
-      -D CMAKE_INSTALL_PREFIX=install \
       -DCMAKE_TOOLCHAIN_FILE="$OPENVINO_HOME/cmake/$TOOLCHAIN_DEFS" \
-      -DCMAKE_STAGING_PREFIX=$STAGING_DIR/opencv \
       -DWITH_GTK_2_X=OFF \
       -DOPENCV_ENABLE_PKG_CONFIG=ON \
       -DPKG_CONFIG_EXECUTABLE=/usr/bin/${ARCH_NAME}-pkg-config \
       $OPENCV_HOME && \
 make -j$BUILD_JOBS && \
-make install && \
+cmake -DCMAKE_INSTALL_PREFIX=$STAGING_DIR/opencv -P cmake_install.cmake && \
 echo export OpenCV_DIR=\$INSTALLDIR/opencv/cmake > $STAGING_DIR/opencv/setupvars.sh && \
 echo export LD_LIBRARY_PATH=\$INSTALLDIR/opencv/lib:\$LD_LIBRARY_PATH >> $STAGING_DIR/opencv/setupvars.sh && \
 cd $DEV_HOME || fail 11 "OpenCV build failed. Stopping"
@@ -132,11 +131,15 @@ cmake -DOpenCV_DIR=$STAGING_DIR/opencv/cmake -DENABLE_OPENCV=OFF \
       -DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath-link,$STAGING_DIR/opencv/lib -DCMAKE_INSTALL_LIBDIR=lib \
       -DENABLE_SSE42=OFF -DENABLE_MYRIAD=ON -DENABLE_GNA=OFF -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
       -DTHREADING=SEQ -DENABLE_LTO=ON \
+      -DTHREADS_PTHREAD_ARG="-pthread" \
       -DCMAKE_CXX_FLAGS=-latomic \
       -DCMAKE_TOOLCHAIN_FILE="$OPENVINO_HOME/cmake/$TOOLCHAIN_DEFS" \
-      -DCMAKE_STAGING_PREFIX=$STAGING_DIR \
+      -DCMAKE_INSTALL_PREFIX=$STAGING_DIR \
+      -DARM_COMPUTE_SCONS_JOBS=$BUILD_JOBS \
+      -DIE_EXTRA_MODULES=$ARM_PLUGIN_HOME \
       $OPENVINO_HOME && \
 make -j$BUILD_JOBS && \
+cmake -DCMAKE_INSTALL_PREFIX=$STAGING_DIR -P cmake_install.cmake && \
 ARCHDIR=`ls $OPENVINO_HOME/bin` && \
 cd $DEV_HOME || fail 12 "OpenVINO build failed. Stopping"
 
@@ -149,32 +152,11 @@ cmake -DInferenceEngineDeveloperPackage_DIR=$OPENVINO_HOME/build \
       -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DENABLE_DATA=OFF \
       -DCMAKE_EXE_LINKER_FLAGS=-Wl,-rpath-link,$STAGING_DIR/opencv/lib \
       -DCMAKE_TOOLCHAIN_FILE="$OPENVINO_HOME/cmake/$TOOLCHAIN_DEFS" \
-      -DCMAKE_STAGING_PREFIX=$STAGING_DIR \
+      -DTHREADS_PTHREAD_ARG="-pthread" \
       $OPENVINO_HOME/inference-engine/ie_bridges/python && \
 make -j$BUILD_JOBS && \
+cmake -DCMAKE_INSTALL_PREFIX=$STAGING_DIR -P cmake_install.cmake && \
 cd $DEV_HOME || fail 13 "OpenVINO python bindings build failed. Stopping"
-
-#ArmCPU plugin
-mkdir -p $ARM_PLUGIN_HOME/build && \
-cd $ARM_PLUGIN_HOME/build && \
-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DInferenceEngineDeveloperPackage_DIR=$OPENVINO_HOME/build \
-      -DENABLE_TESTS=ON -DENABLE_BEH_TESTS=ON -DENABLE_FUNCTIONAL_TESTS=ON \
-      -DARM_COMPUTE_SCONS_JOBS=$BUILD_JOBS \
-      -DCMAKE_TOOLCHAIN_FILE="$OPENVINO_HOME/cmake/$TOOLCHAIN_DEFS" \
-      -DCMAKE_STAGING_PREFIX=$STAGING_DIR \
-      $ARM_PLUGIN_HOME && \
-make -j$BUILD_JOBS && \
-cd $DEV_HOME || fail 14 "ARM plugin build failed. Stopping"
-
-#OpenVINO NGraph deployment
-cd $OPENVINO_HOME/build/ngraph && \
-mkdir -p $STAGING_DIR/deployment_tools && \
-cmake -DCOMPONENT=ngraph \
-      -DCMAKE_STAGING_PREFIX=$STAGING_DIR \
-      -DCMAKE_INSTALL_PREFIX=$STAGING_DIR \
-      -P cmake_install.cmake && \
-cp $OPENVINO_HOME/bin/$ARCHDIR/$BUILD_TYPE/lib/libinterpreter_backend.so $STAGING_DIR/deployment_tools/ngraph/lib/libinterpreter_backend.so && \
-cd $DEV_HOME || fail 15 "OpenVINO NGraph deployment failed. Stopping"
 
 #Open Model Zoo
 if [ "$WITH_OMZ_DEMO" = "ON" ]; then
@@ -190,38 +172,10 @@ if [ "$WITH_OMZ_DEMO" = "ON" ]; then
         $OMZ_HOME/demos && \
   cmake --build $OMZ_DEMOS_BUILD -- -j$BUILD_JOBS && \
   cd $DEV_HOME || fail 16 "Open Model Zoo build failed. Stopping"
+  mkdir -p $STAGING_DIR/deployment_tools/inference_engine/demos) && \
+  cp -vr $OMZ_DEMOS_BUILD $STAGING_DIR/deployment_tools/inference_engine/demos) || \
+  fail 21 "Open Model Zoo package structure preparation failed. Stopping"
 fi
-
-#Package structure creation
-mkdir -p $STAGING_DIR/deployment_tools/inference_engine/bin/$ARCHDIR && \
-find $OPENVINO_HOME/bin/$ARCHDIR/$BUILD_TYPE -maxdepth 1 -type f -exec cp -v {} $STAGING_DIR/deployment_tools/inference_engine/bin/$ARCHDIR \; && \
-mkdir -p $STAGING_DIR/deployment_tools/inference_engine/lib/$ARCHDIR && \
-find $OPENVINO_HOME/bin/$ARCHDIR/$BUILD_TYPE/lib -maxdepth 1 -type f -exec cp -v {} $STAGING_DIR/deployment_tools/inference_engine/lib/$ARCHDIR \; && \
-cp -vr $OPENVINO_HOME/bin/$ARCHDIR/$BUILD_TYPE/lib/python_api $STAGING_DIR/python && \
-cp -vr $OPENVINO_HOME/build/share $STAGING_DIR/deployment_tools/inference_engine/ && \
-cp -v $OPENVINO_HOME/inference-engine/scripts/dependencies.* $OPENVINO_HOME/build/dependencies_64.txt $STAGING_DIR/ && \
-mkdir -p $STAGING_DIR/python/${PYTHONVER}/openvino && \
-cp -vr $OPENVINO_HOME/tools $STAGING_DIR/python/${PYTHONVER}/openvino/ && \
-cp -vr $OPENVINO_HOME/inference-engine/include $STAGING_DIR/deployment_tools/inference_engine/ && \
-cp -vr $OPENVINO_HOME/inference-engine/ie_bridges/c/include/c_api $STAGING_DIR/deployment_tools/inference_engine/include/ && \
-mkdir -p $STAGING_DIR/deployment_tools/inference_engine/samples && \
-cp -vr $OPENVINO_HOME/inference-engine/samples $STAGING_DIR/deployment_tools/inference_engine/samples/cpp && \
-cp -vr $OPENVINO_HOME/inference-engine/ie_bridges/c/samples $STAGING_DIR/deployment_tools/inference_engine/samples/c && \
-cp -vr $OPENVINO_HOME/inference-engine/ie_bridges/python/sample $STAGING_DIR/deployment_tools/inference_engine/samples/python && \
-cp -vr $OPENVINO_HOME/inference-engine/tools $STAGING_DIR/deployment_tools/tools && \
-find $OPENVINO_HOME/model-optimizer -maxdepth 1 -type f -exec cp -v {} $STAGING_DIR/deployment_tools/model-optimizer \; && \
-find $OPENVINO_HOME/model-optimizer/.coveragerc -maxdepth 1 -type f -exec cp -v {} $STAGING_DIR/deployment_tools/model-optimizer \; && \
-cp -vr $OPENVINO_HOME/scripts/setupvars $STAGING_DIR/bin && \
-cp -vr $OPENVINO_HOME/scripts/demo $STAGING_DIR/deployment_tools/demo && \
-cp -vr $OPENVINO_HOME/scripts/install_dependencies $STAGING_DIR/install_dependencies && \
-cp -vr $OPENVINO_HOME/inference-engine/tools $STAGING_DIR/deployment_tools/python_tools && \
-(! [ "$WITH_OMZ_DEMO" = "ON" ] || mkdir -p $STAGING_DIR/deployment_tools/inference_engine/demos) && \
-(! [ "$WITH_OMZ_DEMO" = "ON" ] || cp -vr $OMZ_DEMOS_BUILD $STAGING_DIR/deployment_tools/inference_engine/demos) && \
-echo "=================================RPATH cleaning==================================" && \
-find $STAGING_DIR/deployment_tools/inference_engine/lib/$ARCHDIR/ -maxdepth 1 -type f -name "*.so" -exec chrpath --delete {} \; && \
-find $STAGING_DIR/deployment_tools/inference_engine/bin/$ARCHDIR/ -maxdepth 1 -type f -exec chrpath --delete {} \; && \
-find $STAGING_DIR/python/${PYTHONVER}/openvino/inference_engine/ -maxdepth 1 -type f -name "*.so" -exec chrpath --delete {} \; || \
-fail 21 "Package structure preparation failed. Stopping"
 
 #Package creation
 mkdir -p $DEV_HOME/pack && \
