6fe7c5fb5dc1683e0123e1f41c7982baf50f3826
Yaroslav Torzuk <yaroslav.torzuk2@altran.com>
2021-08-04 14:29:04 +0300

    Moves pure CUDA sigmoid implementation to ops/examples
    Extracts cuDNN descriptor builder from relu.cpp
    
    Adds cuDNN-based sigmoid op implementation
    
    Enables relu single layer tests
    
    Adds single layer tests for sigmoid op
    
    Adds benchmark test for sigmoid
    
    Updates documentation for sigmoid
    
    Adds --exclude-models option to the shape extractor utility
    
    Adds --exclude-models option usage example in shape extractor utility

---
+++ modules/cuda_plugin/src/cuda/descriptor_utils.cpp
@@ -0,0 +1,40 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "descriptor_utils.hpp"
+#include <ops/converters.hpp>
+#include <array>
+
+namespace CUDA {
+
+std::array<int, 5> toArray(const ngraph::Shape& shape) {
+    std::array<int, 5> a { 1, 1, 1, 1, 1 };
+    if (shape.empty())
+        return a;
+    for (std::size_t i = std::min(shape.size(), a.size()); i > 0;) {
+        i--;
+        a[i] = shape[i];
+    }
+    return a;
+}
+
+DnnTensorDescriptor makeDnnTensorDescr(const ngraph::element::Type& type,
+        const ngraph::Shape& shape) { // TODO: different ops have different shape limitations
+    auto dims = toArray(shape);
+    decltype(dims) strides;
+    strides.back() = 1;
+    for (int i = dims.size() - 1; i > 0; i--)
+        strides[i - 1] = strides[i] * dims[i];
+    return {CUDAPlugin::convertDataType<cudnnDataType_t>(type), dims.size(), dims.data(), strides.data()};
+}
+
+CUDA::DnnTensorDescriptor makeInputDnnTensorDescr(const ngraph::Node& node, int n) {
+  return makeDnnTensorDescr(node.get_input_element_type(n), node.get_input_shape(n));
+}
+
+CUDA::DnnTensorDescriptor makeOutputDnnTensorDescr(const ngraph::Node& node, int n) {
+  return makeDnnTensorDescr(node.get_output_element_type(n), node.get_output_shape(n));
+}
+
+}
