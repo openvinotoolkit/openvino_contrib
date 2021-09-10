bdd35ad594ebf3d7240364eb5abbc1c759adeb0f
Andrii Pavliuk <andrii.andrii@altran.com>
2021-09-10 13:02:59 +0300

    Add support for Gather v7
    Add Gather v7 tests

--- modules/cuda_plugin/src/ops/gather.cu
+++ modules/cuda_plugin/src/ops/gather.cu
@@ -27,11 +27,17 @@ constexpr unsigned ELS_PER_THREAD_DICTS = 1;
 
 namespace kernel {
 
-template<typename DataType, typename IndexType>
-static inline __device__ void gather(
-        unsigned data_length, size_t index_range, unsigned els_per_thread,
-        const DataType* src_dict, const IndexType* src_index, DataType* dst_data,
-        unsigned indices_size, unsigned indices_index, unsigned dict, unsigned chunk) {
+template <typename DataType, typename IndexType>
+static inline __device__ void gather(unsigned data_length,
+                                     size_t index_range,
+                                     unsigned els_per_thread,
+                                     unsigned indices_size,
+                                     unsigned indices_index,
+                                     unsigned dict,
+                                     unsigned chunk,
+                                     const DataType* src_dict,
+                                     const IndexType* src_index,
+                                     DataType* dst_data) {
     const auto dict_index = src_index[indices_index];
     if (dict_index >= index_range) {
         // TODO: find a way to handle an error raised in a kernel (assertion or trap) properly
@@ -48,22 +54,37 @@ static inline __device__ void gather(
     }
 }
 
-template<typename DataType, typename IndexType>
-static __global__ void chunks_gather(
-        unsigned data_length, size_t index_range,
-        const DataType* src_dict, const IndexType* src_index, DataType* dst_data) {
+template <typename DataType, typename IndexType>
+static __global__ void chunks_gather(unsigned data_length,
+                                     size_t index_range,
+                                     unsigned num_dicts,
+                                     unsigned dicts_batch_stride,
+                                     unsigned indices_batch_stride,
+                                     unsigned out_batch_stride,
+                                     const DataType* src_dict,
+                                     const IndexType* src_index,
+                                     DataType* dst_data) {
     const auto indices_size = gridDim.y;
     const auto indices_index = blockIdx.y;
-    const auto dict = blockIdx.x;
+    const auto dict = blockIdx.x % num_dicts;
+    const auto batch = blockIdx.x / num_dicts;
     const auto chunk = (blockIdx.z * blockDim.x + threadIdx.x) * ELS_PER_THREAD_CHUNKS;
-    gather(data_length, index_range, ELS_PER_THREAD_CHUNKS,
-           src_dict, src_index, dst_data,
-           indices_size, indices_index, dict, chunk);
+    gather(data_length,
+           index_range,
+           ELS_PER_THREAD_CHUNKS,
+           indices_size,
+           indices_index,
+           dict,
+           chunk,
+           src_dict + batch * dicts_batch_stride,
+           src_index + batch * indices_batch_stride,
+           dst_data + batch * out_batch_stride);
 }
 
 template<typename DataType, typename IndexType>
 static __global__ void dicts_gather(
         unsigned data_length, size_t index_range, unsigned num_dicts,
+        unsigned dicts_batch_stride, unsigned indices_batch_stride, unsigned out_batch_stride,
         const DataType* src_dict, const IndexType* src_index, DataType* dst_data) {
     const auto indices_size = gridDim.y;
     const auto indices_index = blockIdx.y;
@@ -71,10 +92,18 @@ static __global__ void dicts_gather(
     if (dict >= num_dicts) {
         return;
     }
-    const auto chunk = blockIdx.x * ELS_PER_THREAD_DICTS;
-    gather(data_length, index_range, ELS_PER_THREAD_DICTS,
-           src_dict, src_index, dst_data,
-           indices_size, indices_index, dict, chunk);
+    const auto chunk = blockIdx.x % data_length * ELS_PER_THREAD_DICTS;
+    const auto batch = blockIdx.x / data_length;
+    gather(data_length,
+           index_range,
+           ELS_PER_THREAD_DICTS,
+           indices_size,
+           indices_index,
+           dict,
+           chunk,
+           src_dict + batch * dicts_batch_stride,
+           src_index + batch * indices_batch_stride,
+           dst_data + batch * out_batch_stride);
 }
 
 }  // namespace kernel
@@ -83,11 +112,20 @@ GatherOp::GatherOp(const CUDA::CreationContext& context,
                    const ngraph::Node& node,
                    IndexCollection&& inputIds,
                    IndexCollection&& outputIds)
-        : OperationBase(context, node, std::move(inputIds), std::move(outputIds)) {
-    Expects(dynamic_cast<const ngraph::op::v1::Gather*>(&node) != nullptr);
+    : OperationBase(context, node, std::move(inputIds), std::move(outputIds)) {
     Expects(node.get_input_size() == 3);
     Expects(node.get_output_size() == 1);
-    
+    const auto gather_v7 = dynamic_cast<const ngraph::op::v7::Gather*>(&node);
+    const auto gather_v1 = dynamic_cast<const ngraph::op::v1::Gather*>(&node);
+    Expects(gather_v7 || gather_v1);
+
+    const auto gather_base = dynamic_cast<const ngraph::op::util::GatherBase*>(&node);
+    Expects(gather_base);
+
+    // For now CUDA operators support only static shapes
+    Expects(node.get_input_partial_shape(0).rank().is_static() && node.get_input_partial_shape(1).rank().is_static() &&
+            node.get_input_partial_shape(2).rank().is_static());
+
     element_type_ = node.get_input_element_type(0);
     switch (element_type_) {
         case ngraph::element::Type_t::undefined:
@@ -99,40 +137,59 @@ GatherOp::GatherOp(const CUDA::CreationContext& context,
     }
     Expects(node.get_output_element_type(0) == element_type_);
 
-    const auto& params_shape = node.get_input_shape(0);
-    const auto& params_shape_size = params_shape.size();
+    const auto& dict_shape = node.get_input_shape(0);
+    const auto& dict_shape_size = dict_shape.size();
     const auto& indices_shape = node.get_input_shape(1);
     const auto& out_shape = node.get_output_shape(0);
     const auto axis_shape = node.get_input_shape(2);
 
-    Expects(axis_shape.size() == 0 || axis_shape.size() == 1);
-    const auto axis_node = dynamic_cast<ngraph::op::v0::Constant*>(node.get_input_node_ptr(2));
-    Expects(axis_node);
-
     indices_type_ = node.get_input_element_type(1);
-    int64_t raw_axis;
-    switch (indices_type_) {
-        case ngraph::element::Type_t::i64: raw_axis = *axis_node->get_data_ptr<int64_t>(); break;
-        case ngraph::element::Type_t::i32: raw_axis = *axis_node->get_data_ptr<int32_t>(); break;
-        default: throwIEException(
-                fmt::format("Params index type = {} is not supported by Gather operation!",
-                            indices_type_));
+    if (indices_type_ != ngraph::element::Type_t::i64 && indices_type_ != ngraph::element::Type_t::i32) {
+        throwIEException(fmt::format("Params index type = {} is not supported by Gather operation!", indices_type_));
     }
-    const unsigned axis = raw_axis >= 0 ? raw_axis : raw_axis + params_shape_size;
-    Expects(axis < params_shape_size);
 
-    num_dicts_ = std::accumulate(params_shape.cbegin(), params_shape.cbegin() + axis, 1,
-                                 std::multiplies<unsigned>());
-    index_range_ = params_shape[axis];
-    data_length_ = std::accumulate(params_shape.cbegin() + axis + 1, params_shape.end(), 1,
-                                   std::multiplies<unsigned>());
+    const auto axis = gather_base->get_axis();
+    Expects(axis >= 0 && axis < dict_shape_size);
+
+    int64_t batch_dims = 0;
+    if (gather_v7) {
+        batch_dims = gather_v7->get_batch_dims();
+        Expects(batch_dims >= 0 && batch_dims < dict_shape_size && batch_dims < indices_shape.size() &&
+                batch_dims <= axis);
+
+        bool batch_check_ok = true;
+        for (int i = 0; i < batch_dims; ++i) {
+            if (dict_shape[i] != indices_shape[i]) {
+                batch_check_ok = false;
+                break;
+            }
+        }
+        Expects(batch_check_ok);
+    }
+
+    num_dicts_ =
+        std::accumulate(dict_shape.cbegin() + batch_dims, dict_shape.cbegin() + axis, 1, std::multiplies<unsigned>());
+    index_range_ = dict_shape[axis];
+    data_length_ = std::accumulate(dict_shape.cbegin() + axis + 1, dict_shape.cend(), 1, std::multiplies<unsigned>());
 
     if (data_length_ == 0) {
         throwIEException("data_length_ == 0: incorrect input parameters dimension!");
     }
 
-    indices_size_ = ngraph::shape_size(indices_shape);
-    const auto out_size = ngraph::shape_size(out_shape);
+    indices_size_ =
+        std::accumulate(indices_shape.cbegin() + batch_dims, indices_shape.cend(), 1, std::multiplies<unsigned>());
+    const auto out_size =
+        std::accumulate(out_shape.cbegin() + batch_dims, out_shape.cend(), 1, std::multiplies<unsigned>());
+
+    const auto batch_count =
+        std::accumulate(dict_shape.cbegin(), dict_shape.cbegin() + batch_dims, 1, std::multiplies<unsigned>());
+    dicts_batch_stride_ =
+        std::accumulate(dict_shape.cbegin() + batch_dims, dict_shape.cend(), 1, std::multiplies<unsigned>());
+    indices_batch_stride_ =
+        std::accumulate(indices_shape.cbegin() + batch_dims, indices_shape.cend(), 1, std::multiplies<unsigned>());
+    out_batch_stride_ =
+        std::accumulate(out_shape.cbegin() + batch_dims, out_shape.cend(), 1, std::multiplies<unsigned>());
+
     const auto max_indices_index = indices_size_ - 1;
     const auto max_dict_index = num_dicts_ - 1;
 
@@ -155,8 +212,9 @@ GatherOp::GatherOp(const CUDA::CreationContext& context,
                                             ? num_chunks / max_block_size
                                             : num_chunks / max_block_size + 1;
         threads_per_block_ = blocks_per_grid_ == 1 ? num_chunks : max_block_size;
+        grid_dim_x_ = num_dicts_ * batch_count;
 
-        Expects(num_dicts_ <= max_grid_size[0]);
+        Expects(grid_dim_x_ <= max_grid_size[0]);
         Expects(indices_size_ <= max_grid_size[1]);
         Expects(blocks_per_grid_ <= max_grid_size[2]);
     } else {
@@ -164,8 +222,9 @@ GatherOp::GatherOp(const CUDA::CreationContext& context,
                                             ? num_dicts_ / max_block_size
                                             : num_dicts_ / max_block_size + 1;
         threads_per_block_ = blocks_per_grid_ == 1 ? num_dicts_ : max_block_size;
+        grid_dim_x_ = data_length_ * batch_count;
 
-        Expects(data_length_ <= max_grid_size[0]);
+        Expects(grid_dim_x_ <= max_grid_size[0]);
         Expects(indices_size_ <= max_grid_size[1]);
         Expects(blocks_per_grid_ <= max_grid_size[2]);
     }
@@ -231,21 +290,28 @@ void GatherOp::ExecuteImpl(const InferenceRequestContext& context, Inputs inputs
     auto dst_data = static_cast<DataType*>(outputs[0].get());
 
     if (gather_chunks_) {
-        dim3 grid { num_dicts_, indices_size_, blocks_per_grid_ };
-        kernel::chunks_gather<<<grid, threads_per_block_, 0, stream>>>(data_length_,
-                                                                       index_range_,
-                                                                       src_dict,
-                                                                       src_index,
-                                                                       dst_data);
+        kernel::chunks_gather<<<{grid_dim_x_, indices_size_, blocks_per_grid_}, threads_per_block_, 0, stream>>>(
+            data_length_,
+            index_range_,
+            num_dicts_,
+            dicts_batch_stride_,
+            indices_batch_stride_,
+            out_batch_stride_,
+            src_dict,
+            src_index,
+            dst_data);
     } else {
-        dim3 grid { data_length_, indices_size_, blocks_per_grid_ };
-        kernel::dicts_gather<<<grid, threads_per_block_, 0, stream>>>(data_length_,
-                                                                      index_range_,
-                                                                      num_dicts_,
-                                                                      src_dict,
-                                                                      src_index,
-                                                                      dst_data);
-                }
+        kernel::dicts_gather<<<{grid_dim_x_, indices_size_, blocks_per_grid_}, threads_per_block_, 0, stream>>>(
+            data_length_,
+            index_range_,
+            num_dicts_,
+            dicts_batch_stride_,
+            indices_batch_stride_,
+            out_batch_stride_,
+            src_dict,
+            src_index,
+            dst_data);
+    }
     // TODO: find a way to handle an error raised in a kernel (assertion or trap) properly in CUDA Plugin
     throwIfError(cudaGetLastError());
 }
