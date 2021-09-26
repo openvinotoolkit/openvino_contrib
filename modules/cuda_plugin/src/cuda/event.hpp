397506aaabc3c2ee39642ff53ccf29ebad6333ad
Serhii Pavlovskyi <serhii.pavlovskyi@altran.com>
2021-09-27 02:21:57 +0300

    cleanup CUDA::Event implementation

--- modules/cuda_plugin/src/cuda/event.hpp
+++ modules/cuda_plugin/src/cuda/event.hpp
@@ -8,27 +8,12 @@
 
 namespace CUDA {
 
-class Event {
-  /* : public UniqueBase<&cudaEventCreate, &cudaEventDestroy, cudaEvent_t>
-   *   does not work: unable to deduce ‘auto’ from ‘& cudaEventCreate’  */
+class Event : public UniqueBase<static_cast<__host__ cudaError_t CUDARTAPI (*)(cudaEvent_t* event)>(cudaEventCreate),
+                                cudaEventDestroy,
+                                cudaEvent_t> {
 public:
-  using Native = cudaEvent_t;
-  Event(Event&& that) : native { that.native } { that.native = {}; }
-  Event operator=(const Event&) = delete;
-  explicit Event(const Stream& stream) {
-    logIfError(cudaEventCreate(&native));
-    logIfError(cudaEventRecord(get(), stream.get()));
-  }
-  ~Event() {
-    if (native != Native{}) logIfError(cudaEventDestroy(native));
-  }
-  Native get() const noexcept { return native; }
-  float elapsedSince(const Event& start) const {
-    float result {std::numeric_limits<float>::quiet_NaN()};
-    logIfError(cudaEventElapsedTime(&result, start.get(), get()));
-    return result;
-  }
-private:
-  Native native;
+    explicit Event(const Stream& stream) { throwIfError(cudaEventRecord(get(), stream.get())); }
+    float elapsedSince(const Event& start) const { return create(cudaEventElapsedTime, start.get(), get()); }
 };
-} //namespace CUDA
+
+}  // namespace CUDA
