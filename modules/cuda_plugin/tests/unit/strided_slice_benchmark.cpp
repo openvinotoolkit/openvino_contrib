9d63ac0590e179286dbada7710fb4e57b4c1767e
Denis Kotov <denys.kotov@altran.com>
2021-09-29 14:54:22 +0300

    Moved cuda_type_traits.hpp inside of CUDAPlugin
    Moved device_pointers.hpp inside of CUDAPlugin
    
    Apply clang-format

--- modules/cuda_plugin/tests/unit/strided_slice_benchmark.cpp
+++ modules/cuda_plugin/tests/unit/strided_slice_benchmark.cpp
@@ -17,8 +17,8 @@
 
 namespace {
 
-using devptr_t = InferenceEngine::gpu::DevicePointer<void*>;
-using cdevptr_t = InferenceEngine::gpu::DevicePointer<const void*>;
+using devptr_t = CUDA::DevicePointer<void*>;
+using cdevptr_t = CUDA::DevicePointer<const void*>;
 
 struct StridedSliceTest : testing::Test {
     using ElementType = float;
