64695e1f2d1433cf34fc127d3b4dd59183128933
Yaroslav Torzuk <yaroslav.torzuk2@altran.com>
2021-09-30 13:51:47 +0300

    Fix blobs of boolean type support
    Add element wise common kernels
    
    Add logical not kernel implementation
    
    Add LogicalNot op implementation
    
    Add single layer test which covers LogicalNot op functionality
    
    Add benchmark for LogicalNot op
    
    Apply clang-format

---
+++ modules/cuda_plugin/src/ops/logical_not.hpp
@@ -0,0 +1,26 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+#pragma once
+
+#include <cuda_operation_base.hpp>
+#include <kernels/logical_not.cuh>
+
+namespace CUDAPlugin {
+
+class LogicalNotOp : public OperationBase {
+public:
+    explicit LogicalNotOp(const CreationContext& context,
+                          const std::shared_ptr<ngraph::Node>& node,
+                          IndexCollection&& inputIds,
+                          IndexCollection&& outputIds);
+    void Execute(const InferenceRequestContext& context,
+                 Inputs inputTensors,
+                 Outputs outputTensors,
+                 const Workbuffers& workbuffers) const override;
+
+private:
+    kernel::LogicalNot kernel_;
+};
+
+}  // namespace CUDAPlugin
