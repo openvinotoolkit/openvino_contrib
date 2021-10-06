f67d78a39dad211ebfb994a8668fd15bc9d2adc6
Denis Kotov <denys.kotov@altran.com>
2021-10-06 17:46:34 +0300

    Added header file for ReluOp and SigmoidOp

--- modules/cuda_plugin/src/ops/sigmoid.cpp
+++ modules/cuda_plugin/src/ops/sigmoid.cpp
@@ -1,25 +1,19 @@
 // Copyright (C) 2021 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
-#include <cuda/dnn.hpp>
+
+#include "sigmoid.hpp"
+
 #include <cuda_operation_registry.hpp>
 
-#include "activation_forward_cudnn_base.hpp"
-
 namespace CUDAPlugin {
-namespace {
 
-class Sigmoid : public ActivationForwardCuDnnOpBase {
-public:
-    Sigmoid(const CreationContext& context,
-            const std::shared_ptr<ngraph::Node>& node,
-            IndexCollection&& inputIds,
-            IndexCollection&& outputIds)
-        : ActivationForwardCuDnnOpBase{
-              std::make_unique<CUDA::SigmoidDescriptor>(), context, *node, move(inputIds), move(outputIds)} {}
-};
+SigmoidOp::SigmoidOp(const CreationContext& context,
+                     const std::shared_ptr<ngraph::Node>& node,
+                     IndexCollection&& inputIds,
+                     IndexCollection&& outputIds)
+    : ActivationForwardCuDnnOpBase{
+          std::make_unique<CUDA::SigmoidDescriptor>(), context, *node, move(inputIds), move(outputIds)} {}
 
-}  // namespace
-
-OPERATION_REGISTER(Sigmoid, Sigmoid);
+OPERATION_REGISTER(SigmoidOp, Sigmoid);
 }  // namespace CUDAPlugin
