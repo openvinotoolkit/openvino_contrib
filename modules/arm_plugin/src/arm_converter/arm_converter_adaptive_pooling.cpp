64d4103df3cd71c257acd50e516f5c52ed79c7e0
rayonnant14 <smolpolina199906@icloud.com>
2021-10-07 22:33:05 +0300

    added adaptive_pooling reference implementation

---
+++ modules/arm_plugin/src/arm_converter/arm_converter_adaptive_pooling.cpp
@@ -0,0 +1,38 @@
+// Copyright (C) 2020-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+
+#include <ngraph/runtime/reference/adaptive_avg_pool.hpp>
+#include <ngraph/runtime/reference/adaptive_max_pool.hpp>
+#include "arm_converter/arm_converter.hpp"
+
+namespace ArmPlugin {
+template<> Converter::Conversion::Ptr Converter::Convert(const ngraph::op::v8::AdaptiveAvgPool& node) {
+    auto make = [&] (auto refFunction) {
+    return this->MakeConversion(refFunction,
+                                node.input(0),
+                                node.output(0),
+                                node.get_input_shape(0),
+                                node.get_output_shape(0));
+    };
+    return CallSwitch(
+        AP_WRAP(make, ngraph::runtime::reference::adaptive_avg_pool),
+        node.input(0), floatTypes);
+}
+
+template<> Converter::Conversion::Ptr Converter::Convert(const ngraph::op::v8::AdaptiveMaxPool& node) {
+    auto make = [&] (auto refFunction) {
+    return this->MakeConversion(refFunction,
+                                node.input(0),
+                                node.output(0),
+                                node.input(1),
+                                node.get_input_shape(0),
+                                node.get_output_shape(0));
+    };
+    return CallSwitch(
+        AP_WRAP(make, ngraph::runtime::reference::adaptive_max_pool),
+        node.input(0), floatTypes,
+        node.input(1), indexTypes);
+}
+
+} // namespace ArmPlugin
