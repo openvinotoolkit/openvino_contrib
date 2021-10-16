1576601bb1b912996ce74b5e2494e3cc88d747fe
Alexander Nesterov <nesoldr@gmail.com>
2021-10-17 02:45:48 +0300

    Init project

---
+++ modules/arm_plugin/samples/face_recognition_demo/app/src/main/java/com/openvino_demo/face_recognition_demo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.openvino_demo.face_recognition_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
