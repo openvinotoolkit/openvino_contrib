db1aa45d35594d6a2db832bec8b98a2c8a11db7e
vitaliy.mostovy <vitaliy.mostovy@altran.com>
2021-10-19 12:57:18 +0300

    Use OperationRegistry factory to create Multiply operation.
    Implement Multiply for integer tensors
    
    Add test for Tacotron2 encoder Multiply operation.
    
    Apply clang format

--- modules/cuda_plugin/src/ops/multiply.cpp
+++ modules/cuda_plugin/src/ops/multiply.cpp
@@ -1,18 +1,41 @@
 // Copyright (C) 2021 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
-#include "multiply.hpp"
 
-#include <cuda_operation_registry.hpp>
+#include <fmt/format.h>
+
+#include <gsl/gsl_assert>
+#include <sstream>
+
+#include "cuda_operation_registry.hpp"
+#include "multiply_cuda.hpp"
+#include "multiply_cudnn.hpp"
 
 namespace CUDAPlugin {
 
-MultiplyOp::MultiplyOp(const CreationContext& context,
-                       const std::shared_ptr<ngraph::Node>& node,
-                       IndexCollection&& inputIds,
-                       IndexCollection&& outputIds)
-    : CuDnnTensorOpBase{context, node, move(inputIds), move(outputIds), cudnnOpTensorOp_t::CUDNN_OP_TENSOR_MUL} {}
+static OperationBase::Ptr multiplyFactory(const CreationContext& context,
+                                          const std::shared_ptr<ngraph::Node>& node,
+                                          OperationBase::IndexCollection&& inputIds,
+                                          OperationBase::IndexCollection&& outputIds) {
+    const OperationBase::IndexCollection inputs{inputIds};
+    const OperationBase::IndexCollection outputs{outputIds};
 
-OPERATION_REGISTER(MultiplyOp, Multiply);
+    std::stringstream exception_msg;
+    try {
+        return std::make_shared<MultiplyCuDnnOp>(
+            context, node, OperationBase::IndexCollection{inputs}, OperationBase::IndexCollection{outputs});
+    } catch (const std::exception& e) {
+        exception_msg << "Failed to create MultiplyCuDnn impl: " << e.what();
+    }
+    try {
+        return std::make_shared<MultiplyCudaOp>(
+            context, *node, OperationBase::IndexCollection{inputs}, OperationBase::IndexCollection{outputs});
+    } catch (const std::exception& e) {
+        exception_msg << "\nFailed to create MultiplyCuda impl: " << e.what();
+    }
+    throwIEException(fmt::format("Multiply node is not supported:\n{}", exception_msg.str()));
+}
+
+OPERATION_REGISTER_FACTORY(Multiply, multiplyFactory)
 
 }  // namespace CUDAPlugin
