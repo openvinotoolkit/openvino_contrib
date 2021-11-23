c97c9777799b08409daf598f2e57f7e311d92182
rayonnant14 <smolpolina199906@icloud.com>
2021-11-23 23:19:08 +0300

    added color_convert_i420 reference support
    fixed strides calculations in nv12

--- modules/arm_plugin/src/arm_converter/arm_converter_convert_color_i420.cpp
+++ modules/arm_plugin/src/arm_converter/arm_converter_convert_color_i420.cpp
@@ -7,24 +7,26 @@
 
 namespace ArmPlugin {
 template <typename T>
-void wrap_color_convert_nv12(const T* arg_y,
+void wrap_color_convert_i420(const T* arg_y,
                              const ngraph::Shape& arg_y_shape,
-                             const T* arg_uv,
+                             const T* arg_u,
+                             const T* arg_v,
                              T* out_ptr,
-                             const ov::op::util::ConvertColorNV12Base::ColorConversion& color_format,
+                             ov::op::util::ConvertColorI420Base::ColorConversion color_format,
                              const bool single_plane) {
     enum Arg_y {N_DIM, H_DIM, W_DIM, C_DIM};
     size_t batch_size = arg_y_shape[Arg_y::N_DIM];
     size_t image_w = arg_y_shape[Arg_y::W_DIM];
     size_t image_h = arg_y_shape[Arg_y::H_DIM];
     size_t stride_y =  image_w * image_h;
-    size_t stride_uv =  image_w * image_h / 2;
+    size_t stride_uv =  image_w * image_h / 4;
     if (single_plane) {
         image_h = image_h * 2 / 3;
-        stride_uv = stride_uv * 2;
+        stride_uv = stride_uv * 4;
     }
-    ngraph::runtime::reference::color_convert_nv12<T>(arg_y,
-                                                      single_plane ? arg_uv + image_w * image_h : arg_uv,
+    ngraph::runtime::reference::color_convert_i420<T>(arg_y,
+                                                      single_plane ? arg_u + image_w * image_h : arg_u,
+                                                      single_plane ? arg_v + 5 * image_w * image_h / 4 : arg_v,
                                                       out_ptr,
                                                       batch_size,
                                                       image_h,
@@ -34,36 +36,38 @@ void wrap_color_convert_nv12(const T* arg_y,
                                                       color_format);
 }
 
-template<> Converter::Conversion::Ptr Converter::Convert(const ngraph::op::v8::NV12toBGR& node) {
+template<> Converter::Conversion::Ptr Converter::Convert(const ngraph::op::v8::I420toBGR& node) {
     const bool single_plane = node.get_input_size() == 1;
     auto make = [&] (auto refFunction) {
         return this->MakeConversion(refFunction,
                                     node.input(0),
                                     node.get_input_shape(0),
                                     single_plane ? node.input(0) : node.input(1),
+                                    single_plane ? node.input(0) : node.input(2),
                                     node.output(0),
-                                    ov::op::util::ConvertColorNV12Base::ColorConversion::NV12_TO_BGR,
+                                    ov::op::util::ConvertColorI420Base::ColorConversion::I420_TO_BGR,
                                     single_plane);
         };
     return CallSwitch(
-        AP_WRAP(make, wrap_color_convert_nv12),
+        AP_WRAP(make, wrap_color_convert_i420),
         node.get_input_element_type(0),
         std::tuple<std::uint8_t, ngraph::float16, float>{});
 }
 
-template<> Converter::Conversion::Ptr Converter::Convert(const ngraph::op::v8::NV12toRGB& node) {
+template<> Converter::Conversion::Ptr Converter::Convert(const ngraph::op::v8::I420toRGB& node) {
     const bool single_plane = node.get_input_size() == 1;
     auto make = [&] (auto refFunction) {
         return this->MakeConversion(refFunction,
                                     node.input(0),
                                     node.get_input_shape(0),
                                     single_plane ? node.input(0) : node.input(1),
+                                    single_plane ? node.input(0) : node.input(2),
                                     node.output(0),
-                                    ov::op::util::ConvertColorNV12Base::ColorConversion::NV12_TO_RGB,
+                                    ov::op::util::ConvertColorI420Base::ColorConversion::I420_TO_RGB,
                                     single_plane);
         };
     return CallSwitch(
-        AP_WRAP(make, wrap_color_convert_nv12),
+        AP_WRAP(make, wrap_color_convert_i420),
         node.get_input_element_type(0),
         std::tuple<std::uint8_t, ngraph::float16, float>{});
 }
