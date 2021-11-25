211ef98b0b93a39e1a49a4dcfaf0c384e4d7a584
tomdol <tomasz.dolbniak@intel.com>
2021-11-25 17:10:52 +0100

    Use get_ie_output_name from OV repository

--- modules/arm_plugin/src/transformations/store_result_name.cpp
+++ modules/arm_plugin/src/transformations/store_result_name.cpp
@@ -3,15 +3,15 @@
 
 
 #include "opset/opset.hpp"
-#include "output_names.hpp"
 #include "store_result_name.hpp"
+#include "transformations/utils/utils.hpp"
 
 bool ArmPlugin::pass::StoreResultName::run_on_function(std::shared_ptr<ngraph::Function> f) {
     for (auto&& node : f->get_results()) {
         IE_ASSERT(node->inputs().size() == 1);
         auto input = node->input(0);
         auto sourceOutput = input.get_source_output();
-        const auto outputName = create_ie_output_name(sourceOutput);
+        const auto outputName = ngraph::op::util::get_ie_output_name(sourceOutput);
         node->get_rt_info().emplace("ResultName", std::make_shared<ngraph::VariantWrapper<std::string>>(outputName));
     }
     return false;
