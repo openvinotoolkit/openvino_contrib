8d55d7b818580a0ad8fcdddf8d22dbf533a76570
Oleksandr Kramskyi <oleksandr.kramskyi@altran.com>
2021-12-14 16:08:53 +0200

    Add GroupConvolution

--- modules/cuda_plugin/src/ops/convolution_components.cpp
+++ modules/cuda_plugin/src/ops/convolution_components.cpp
@@ -7,6 +7,7 @@
 #include <gsl/gsl_assert>
 #include <gsl/span_ext>
 #include <ngraph/validation_util.hpp>
+#include <ngraph/op/group_conv.hpp>
 
 namespace CUDAPlugin::Convolution::Details {
 
@@ -21,7 +22,8 @@ ConvolutionParams::ConvolutionParams(const TConvNode& node)
       strides_{node.get_strides()},
       dilations_{node.get_dilations()},
       padding_before_{node.get_pads_begin()},
-      padding_after_{node.get_pads_end()} {
+      padding_after_{node.get_pads_end()},
+      groups_{1U} {
     Expects(input_shape_.size() > NON_SPATIAL_DIMS_NUMBER);
     Expects(element_type_ == node.get_input_element_type(ConvArgIndices::filter));
     Expects(element_type_ == node.get_output_element_type(ConvArgIndices::output));
@@ -29,6 +31,14 @@ ConvolutionParams::ConvolutionParams(const TConvNode& node)
     InferPadding(node);
 
     if (input_shape_.size() == CONV_1D_DIMS_NUMBER) ConvertConv1DToConv2D();
+
+    if constexpr (std::is_same_v<TConvNode, ngraph::op::v1::GroupConvolution>) {
+        groups_ = node.get_input_shape(1)[0];
+        Expects(input_shape_[1] % groups_ == 0);
+        filter_shape_.erase(filter_shape_.begin());
+    }
+    Expects(groups_ >= 1U);
+    filter_shape_[0] *= groups_;
 
     const size_t dims_number = NumberOfDims();
     Ensures(input_shape_.size() == dims_number);
@@ -44,6 +54,7 @@ ConvolutionParams::ConvolutionParams(const TConvNode& node)
     Ensures(padding_before_.size() == spatial_dims_number);
     Ensures(padding_after_.size() == spatial_dims_number);
 }
+template ConvolutionParams::ConvolutionParams(const ngraph::op::v1::GroupConvolution& node);
 template ConvolutionParams::ConvolutionParams(const ngraph::op::v1::Convolution& node);
 template ConvolutionParams::ConvolutionParams(const nodes::FusedConvolution& node);
 
---
+++ modules/cuda_plugin/src/ops/group_convolution.cpp
@@ -0,0 +1,31 @@
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+#include "group_convolution.hpp"
+
+#include <cuda_operation_registry.hpp>
+#include <ngraph/partial_shape.hpp>
+
+#include "ops/convolution_components.hpp"
+
+namespace CUDAPlugin {
+
+GroupConvolutionOp::GroupConvolutionOp(const CreationContext &context,
+                                       const NodeOp &node,
+                                       IndexCollection &&inputIds,
+                                       IndexCollection &&outputIds)
+    : OperationCuDnn{context, node, move(inputIds), move(outputIds)},
+      convolution_(context, node, move(inputIds), move(outputIds), Convolution::Details::ConvolutionParams{node}) {}
+
+void GroupConvolutionOp::Execute(const InferenceRequestContext &context,
+                                 Inputs inputTensors,
+                                 Outputs outputTensors,
+                                 const Workbuffers &buffers) const {
+    convolution_.Execute(context, inputTensors, outputTensors, buffers);
+}
+
+WorkbufferRequest GroupConvolutionOp::GetWorkBufferRequest() const { return convolution_.GetWorkBufferRequest(); }
+
+OPERATION_REGISTER(GroupConvolutionOp, GroupConvolution);
+
+}  // namespace CUDAPlugin
