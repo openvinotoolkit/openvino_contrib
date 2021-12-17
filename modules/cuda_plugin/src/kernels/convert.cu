9dd494b176b3d34462f1b15196be448233d2021e
Oleksii Zaderykhin <ozaderykhin@lohika.com>
2021-12-17 12:31:31 +0200

    The range operation was implemented. Tests for it was added.
    The in_kernel_cast.cuh, type_combinations.cuh were renamed to convert.cuh, typed_functor.hpp respectively. The out of range case for the typed_functor was fixed and correspondent unit test was added.
    
    The base class inheritance was changed from public to private for the typed_functor class.
    
    The t_func_ptr and typed_functor were renamed to TFuncPtr and TypedFunctor respectively. The type_t_first_value and type_t_last_value were added into the cuda_type_traits.hpp. The description for the TypedFunctor was added with examples.
    
    The constant reference argument for integral types was removed. The outputSize_ was changed to output_size_.

--- modules/cuda_plugin/src/kernels/convert.cu
+++ modules/cuda_plugin/src/kernels/convert.cu
@@ -3,46 +3,19 @@
 //
 
 #include <array>
-#include <type_traits>
-
+#include "convert.cuh"
 #include "convert.hpp"
 #include "error.hpp"
 
 namespace CUDAPlugin {
 namespace kernel {
 
-#if CUDA_VERSION >= 11000
 template <typename TOutput, typename TInput>
 __global__
-    typename std::enable_if<std::is_same<TInput, __half>::value || std::is_same<TInput, __nv_bfloat16>::value ||
-                            std::is_same<TOutput, __half>::value || std::is_same<TOutput, __nv_bfloat16>::value>::type
-#else
-template <typename TOutput, typename TInput>
-__global__ typename std::enable_if<std::is_same<TInput, __half>::value || std::is_same<TOutput, __half>::value>::type
-#endif
-    convert_impl(size_t inputSize, TOutput* out, const TInput* in) {
+    void convert_impl(size_t inputSize, TOutput* out, const TInput* in) {
     const size_t i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < inputSize) {
-        // workaround for "error: more than one conversion function from "const __half" to "..." applies"
-        // converting __half, __nv_bfloat16 via float
-        out[i] = static_cast<TOutput>(static_cast<float>(in[i]));
-    }
-}
-
-#if CUDA_VERSION >= 11000
-template <typename TOutput, typename TInput>
-__global__
-    typename std::enable_if<!(std::is_same<TInput, __half>::value || std::is_same<TInput, __nv_bfloat16>::value ||
-                              std::is_same<TOutput, __half>::value ||
-                              std::is_same<TOutput, __nv_bfloat16>::value)>::type
-#else
-template <typename TOutput, typename TInput>
-__global__ typename std::enable_if<!(std::is_same<TInput, __half>::value || std::is_same<TOutput, __half>::value)>::type
-#endif
-    convert_impl(size_t inputSize, TOutput* out, const TInput* in) {
-    const size_t i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < inputSize) {
-        out[i] = static_cast<TOutput>(in[i]);
+        out[i] = cast<TOutput>(in[i]);
     }
 }
 
@@ -56,8 +29,8 @@ struct ConvertFunctor {
                          const void* input,
                          unsigned numBlocks,
                          unsigned threadsPerBlock) {
-        constexpr Type_t output_type = static_cast<Type_t>(OutputType + static_cast<size_t>(Type_t::boolean));
-        constexpr Type_t input_type = static_cast<Type_t>(InputType + static_cast<size_t>(Type_t::boolean));
+        constexpr Type_t output_type = static_cast<Type_t>(OutputType + type_t_first_value);
+        constexpr Type_t input_type = static_cast<Type_t>(InputType + type_t_first_value);
         using TOutput = cuda_type_traits_t<output_type>;
         using TInput = cuda_type_traits_t<input_type>;
         if (OutputType == InputType) {
@@ -72,7 +45,7 @@ struct ConvertFunctor {
 
 using convert_t = Convert::convert_t;
 
-constexpr size_t type_count = static_cast<size_t>(Type_t::u64) - static_cast<size_t>(Type_t::boolean) + 1;
+constexpr size_t type_count = type_t_last_value - type_t_first_value + 1;
 
 template <template <size_t> class Template>
 struct convert_vector : std::array<convert_t, type_count> {
@@ -111,8 +84,8 @@ Convert::Convert(
 
 Convert::convert_t Convert::getConvertKernel(Type_t output_element_type, Type_t input_element_type) {
     static constexpr detail::convert_matrix<detail::ConvertFunctor> matrix{};
-    const size_t input_type_index = static_cast<size_t>(input_element_type) - static_cast<size_t>(Type_t::boolean);
-    const size_t output_type_index = static_cast<size_t>(output_element_type) - static_cast<size_t>(Type_t::boolean);
+    const size_t input_type_index = static_cast<size_t>(input_element_type) - type_t_first_value;
+    const size_t output_type_index = static_cast<size_t>(output_element_type) - type_t_first_value;
     return matrix[output_type_index][input_type_index];
 }
 
