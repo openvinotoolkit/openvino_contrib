f6f57ce1f2884c75b29c8f94ffde8e19886dcc64
Oleksii Zaderykhin <ozaderykhin@lohika.com>
2021-12-29 18:52:32 +0200

    The TypedFunctor was reused in the Convert operation.

--- modules/cuda_plugin/src/kernels/convert.cu
+++ modules/cuda_plugin/src/kernels/convert.cu
@@ -2,10 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include <array>
 #include "convert.cuh"
 #include "convert.hpp"
 #include "error.hpp"
+#include "typed_functor.hpp"
 
 namespace CUDAPlugin {
 namespace kernel {
@@ -19,74 +19,40 @@ __global__
     }
 }
 
-namespace detail {
+template <typename TOutput, typename TInput, typename sfinae_helper = void>
+struct ConvertFunctor;
 
-template <size_t OutputType, size_t InputType>
-struct ConvertFunctor {
+template <typename TOutput, typename TInput>
+struct ConvertFunctor<TOutput, TInput, typename std::enable_if<std::is_same<TOutput, TInput>::value>::type> {
+    static void function(cudaStream_t stream,
+                         size_t size,
+                         void* output,
+                         const void* input,
+                         unsigned /*numBlocks*/,
+                         unsigned /*threadsPerBlock*/) {
+        if (output == input) return;
+        throwIfError(cudaMemcpyAsync(output, input, size * sizeof(TOutput), cudaMemcpyDeviceToDevice, stream));
+    }
+};
+
+template <typename TOutput, typename TInput>
+struct ConvertFunctor<TOutput, TInput, typename std::enable_if<!std::is_same<TOutput, TInput>::value>::type> {
     static void function(cudaStream_t stream,
                          size_t size,
                          void* output,
                          const void* input,
                          unsigned numBlocks,
                          unsigned threadsPerBlock) {
-        constexpr Type_t output_type = static_cast<Type_t>(OutputType + type_t_first_value);
-        constexpr Type_t input_type = static_cast<Type_t>(InputType + type_t_first_value);
-        using TOutput = cuda_type_traits_t<output_type>;
-        using TInput = cuda_type_traits_t<input_type>;
-        if (OutputType == InputType) {
-            if (output == input) return;
-            throwIfError(cudaMemcpyAsync(output, input, size * sizeof(TOutput), cudaMemcpyDeviceToDevice, stream));
-        } else {
-            CUDAPlugin::kernel::convert_impl<TOutput, TInput><<<numBlocks, threadsPerBlock, 0, stream>>>(
-                size, static_cast<TOutput*>(output), static_cast<const TInput*>(input));
-        }
+        CUDAPlugin::kernel::convert_impl<TOutput, TInput><<<numBlocks, threadsPerBlock, 0, stream>>>(
+            size, static_cast<TOutput*>(output), static_cast<const TInput*>(input));
     }
 };
-
-using convert_t = Convert::convert_t;
-
-constexpr size_t type_count = type_t_last_value - type_t_first_value + 1;
-
-template <template <size_t> class Template>
-struct convert_vector : std::array<convert_t, type_count> {
-    constexpr convert_vector() : convert_vector(std::make_index_sequence<type_count>()) {}
-
-private:
-    template <size_t... I>
-    constexpr convert_vector(std::index_sequence<I...>)
-        : std::array<convert_t, type_count>{&Template<I>::function...} {}
-};
-
-template <template <size_t, size_t> class Template, size_t N>
-struct reduce {
-    template <size_t M>
-    using type = Template<N, M>;
-};
-
-template <template <size_t, size_t> class Template>
-class convert_matrix : public std::array<std::array<convert_t, type_count>, type_count> {
-public:
-    constexpr convert_matrix() : convert_matrix<Template>(std::make_index_sequence<type_count>()) {}
-
-private:
-    template <size_t... I>
-    constexpr convert_matrix(std::index_sequence<I...>)
-        : std::array<std::array<convert_t, type_count>, type_count>{
-              convert_vector<reduce<Template, I>::template type>{}...} {}
-};
-}  // namespace detail
 
 Convert::Convert(
     Type_t output_element_type, Type_t input_element_type, size_t size, size_t numBlocks, size_t threadsPerBlock)
     : size_{size}, num_blocks_{numBlocks}, threads_per_block_{threadsPerBlock} {
-    convert_kernel_ = getConvertKernel(output_element_type, input_element_type);
-}
-
-Convert::convert_t Convert::getConvertKernel(Type_t output_element_type, Type_t input_element_type) {
-    static constexpr detail::convert_matrix<detail::ConvertFunctor> matrix{};
-    const size_t input_type_index = static_cast<size_t>(input_element_type) - type_t_first_value;
-    const size_t output_type_index = static_cast<size_t>(output_element_type) - type_t_first_value;
-    return matrix[output_type_index][input_type_index];
+    static constexpr TypedFunctor<ConvertFunctor, convert_t, DIM_2D> combinations{};
+    convert_kernel_ = combinations[output_element_type][input_element_type];
 }
 
 void Convert::operator()(cudaStream_t stream, void* output, const void* src) const {
