472eb14ed589b880419a832d8299d8fed6151c29
Denis Kotov <denys.kotov@altran.com>
2022-01-04 19:56:40 +0200

    Made cuda wrappers C++14 compatible
    Fixed C++14 support in cuda wrappers
    
    Added support for CUDA operators and moved mathematical functions in cuda folder
    
    Added CUDA::Atomic-s
    
    Added CUDA::Pair
    
    Added CUDA::Span
    
    Added CUDA::Array
    
    Added CUDA::MDSpan
    
    Added CUDA::Vector
    
    Added CUDA::MDVector
    
    Added possibility to run CUDA tests
    
    Added sort algorithms
    
    Added DetectionOutput operation
    
    Added tests for DetectionOutput operation
    
    Added using CUDA::Stream and CUDA wrappers in DetectionOutput

--- modules/cuda_plugin/src/cuda/dnn_be_desc.hpp
+++ modules/cuda_plugin/src/cuda/dnn_be_desc.hpp
@@ -5,8 +5,9 @@
 #pragma once
 
 #include <gsl/span>
-#include "runtime.hpp"
+
 #include "dnn_be_attrs.hpp"
+#include "runtime.hpp"
 
 namespace CUDA {
 
@@ -22,84 +23,78 @@ namespace CUDA {
  */
 class DnnBackendDescriptor {
 public:
-    DnnBackendDescriptor(cudnnBackendDescriptorType_t descriptorType)
-        : owner_ {create(descriptorType)} {
-    }
+    DnnBackendDescriptor(cudnnBackendDescriptorType_t descriptorType) : owner_{create(descriptorType)} {}
 
     cudnnBackendDescriptor_t get() const { return owner_.get(); }
 
-    template<cudnnBackendAttributeName_t Name>
+    template <cudnnBackendAttributeName_t Name>
     using ValueType = typename DnnBEAttrTypeID<DnnBEAttrName<Name>::TypeID>::ValueType;
 
-    template<cudnnBackendAttributeType_t TypeID>
+    template <cudnnBackendAttributeType_t TypeID>
     void setAttributeValues(cudnnBackendAttributeName_t name,
                             gsl::span<typename DnnBEAttrTypeID<TypeID>::ValueType> values) {
         throwIfError(::cudnnBackendSetAttribute(get(), name, TypeID, values.size(), values.data()));
     }
 
-    template<cudnnBackendAttributeName_t Name>
+    template <cudnnBackendAttributeName_t Name>
     void setAttributeValues(gsl::span<ValueType<Name>> values) {
         setAttributeValues<DnnBEAttrName<Name>::TypeID>(Name, values);
     }
 
-    template<cudnnBackendAttributeType_t TypeID>
-    void setAttributeValue(cudnnBackendAttributeName_t name,
-                           typename DnnBEAttrTypeID<TypeID>::ValueType value) {
-        setAttributeValues<TypeID>(name, gsl::span<typename DnnBEAttrTypeID<TypeID>::ValueType> {&value, 1});
+    template <cudnnBackendAttributeType_t TypeID>
+    void setAttributeValue(cudnnBackendAttributeName_t name, typename DnnBEAttrTypeID<TypeID>::ValueType value) {
+        setAttributeValues<TypeID>(name, gsl::span<typename DnnBEAttrTypeID<TypeID>::ValueType>{&value, 1});
     }
 
-    template<cudnnBackendAttributeName_t Name>
+    template <cudnnBackendAttributeName_t Name>
     void setAttributeValue(ValueType<Name> value) {
         setAttributeValue<DnnBEAttrName<Name>::TypeID>(Name, value);
     }
 
-    void finalize() {
-        throwIfError(::cudnnBackendFinalize(get()));
-    }
+    void finalize() { throwIfError(::cudnnBackendFinalize(get())); }
 
-    template<cudnnBackendAttributeName_t Name>
+    template <cudnnBackendAttributeName_t Name>
     int64_t getAttributeValueCount() const {
         int64_t num_values = 0;
-        throwIfError(::cudnnBackendGetAttribute(get(), Name,  DnnBEAttrName<Name>::TypeID,
-                                                0, &num_values, nullptr));
+        throwIfError(::cudnnBackendGetAttribute(get(), Name, DnnBEAttrName<Name>::TypeID, 0, &num_values, nullptr));
         return num_values;
     }
 
-    template<cudnnBackendAttributeName_t Name>
+    template <cudnnBackendAttributeName_t Name>
     std::vector<ValueType<Name>> getAttributeValues() const {
         std::vector<ValueType<Name>> values(getAttributeValueCount<Name>());
         getAttributeValues<Name>(values);
         return values;
     }
 
-    template<cudnnBackendAttributeName_t Name, class T,
-             typename = std::enable_if_t<std::is_convertible_v<T*, DnnBackendDescriptor*>>>
+    template <cudnnBackendAttributeName_t Name,
+              class T,
+              typename = typename std::enable_if<std::is_convertible<T*, DnnBackendDescriptor*>::value>::type>
     std::vector<T> getBEDescAttributeValues() const {
         std::vector<T> values(getAttributeValueCount<Name>());
         std::vector<cudnnBackendDescriptor_t> raw_be_descs;
-        std::transform(values.begin(), values.end(), std::back_inserter(raw_be_descs),
-                       std::bind(&T::get, std::placeholders::_1));
+        std::transform(
+            values.begin(), values.end(), std::back_inserter(raw_be_descs), std::bind(&T::get, std::placeholders::_1));
         getAttributeValues<Name>(raw_be_descs);
         values.resize(raw_be_descs.size());
         return values;
     }
 
-    template<cudnnBackendAttributeName_t Name>
+    template <cudnnBackendAttributeName_t Name>
     ValueType<Name> getAttributeValue() const {
         int64_t num_values = 0;
-        ValueType<Name> value {};
-        throwIfError(::cudnnBackendGetAttribute(get(), Name, DnnBEAttrName<Name>::TypeID,
-                                                1, &num_values, &value));
+        ValueType<Name> value{};
+        throwIfError(::cudnnBackendGetAttribute(get(), Name, DnnBEAttrName<Name>::TypeID, 1, &num_values, &value));
         Ensures(1 == num_values);
         return value;
     }
 
 private:
-    template<cudnnBackendAttributeName_t Name>
+    template <cudnnBackendAttributeName_t Name>
     void getAttributeValues(std::vector<ValueType<Name>>& io_values) const {
         int64_t num_values = -1;
-        throwIfError(::cudnnBackendGetAttribute(get(), Name, DnnBEAttrName<Name>::TypeID,
-                                                io_values.size(), &num_values, io_values.data()));
+        throwIfError(::cudnnBackendGetAttribute(
+            get(), Name, DnnBEAttrName<Name>::TypeID, io_values.size(), &num_values, io_values.data()));
         {
             // NOTE: Implementing workaround for cuDNN v8.1 bug, when sometimes the number of actually
             //       returned attributes is smaller than previously returned by `getAttributeValueCount()`.
@@ -112,7 +107,7 @@ private:
 
 private:
     static cudnnBackendDescriptor_t create(cudnnBackendDescriptorType_t descriptorType) {
-        cudnnBackendDescriptor_t desc {};
+        cudnnBackendDescriptor_t desc{};
         throwIfError(::cudnnBackendCreateDescriptor(descriptorType, &desc));
         return desc;
     }
@@ -121,7 +116,7 @@ private:
             logIfError(::cudnnBackendDestroyDescriptor(desc));
         }
     };
-    std::unique_ptr<std::remove_pointer_t<cudnnBackendDescriptor_t>, Deleter> owner_;
+    std::unique_ptr<typename std::remove_pointer<cudnnBackendDescriptor_t>::type, Deleter> owner_;
 };
 
-} // namespace CUDA
+}  // namespace CUDA
