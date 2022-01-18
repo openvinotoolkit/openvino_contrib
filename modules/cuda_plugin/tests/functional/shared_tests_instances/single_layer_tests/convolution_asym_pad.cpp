65e2311b56fb16a5d013221fd0eafe8bf60e80ac
Oleksandr Kramskyi <oleksandr.kramskyi@altran.com>
2022-01-18 12:08:42 +0200

    [Convolution] Add asymmetric padding
    [GroupConvolution] Add asymmetric padding
    
    Add autogenerated tests for GroupConvolution

--- modules/cuda_plugin/tests/functional/shared_tests_instances/single_layer_tests/convolution_asym_pad.cpp
+++ modules/cuda_plugin/tests/functional/shared_tests_instances/single_layer_tests/convolution_asym_pad.cpp
@@ -1,34 +1,29 @@
-// Copyright (C) 2021-2022 Intel Corporation
+// Copyright (C) 2022 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
-#include "shared_test_classes/single_layer/group_convolution.hpp"
-
 #include <cuda_test_constants.hpp>
 #include <vector>
+
+#include "shared_test_classes/single_layer/convolution.hpp"
 
 using namespace LayerTestsDefinitions;
 
 namespace CUDALayerTestsDefinitions {
 
-class GroupConvolutionCUDALayerTest : public GroupConvolutionLayerTest {};
+class ConvolutionAsymPadCUDALayerTest : public ConvolutionLayerTest {};
 
-TEST_P(GroupConvolutionCUDALayerTest, Run) { Run(); }
+TEST_P(ConvolutionAsymPadCUDALayerTest, Run) { Run(); }
 
 namespace {
-const std::vector<InferenceEngine::Precision> precisions = {InferenceEngine::Precision::FP16,
-                                                            InferenceEngine::Precision::FP32};
+const std::vector<InferenceEngine::Precision> precisions = {InferenceEngine::Precision::FP32};
 const std::vector<std::vector<size_t>> kernels = {{3, 3}};
 const std::vector<std::vector<size_t>> strides = {{1, 1}};
-const std::vector<std::vector<ptrdiff_t>> padBegins = {{0, 0}, {0, 2}};
-const std::vector<std::vector<ptrdiff_t>> padEnds = {{0, 0}, {2, 1}};
+const std::vector<std::vector<ptrdiff_t>> padBegins = {{1, 1}};
+const std::vector<std::vector<ptrdiff_t>> padEnds = {{0, 2}};
 const std::vector<std::vector<size_t>> dilations = {{1, 1}};
-const std::vector<size_t> numOutChannels = {8, 16};
-const std::vector<size_t> numGroups = {2, 8};
-const std::vector<ngraph::op::PadType> pad_types = {ngraph::op::PadType::EXPLICIT,
-                                                    ngraph::op::PadType::VALID,
-                                                    ngraph::op::PadType::SAME_UPPER,
-                                                    ngraph::op::PadType::SAME_LOWER};
-const auto inputShapes = std::vector<size_t>({1, 16, 30, 30});
+const std::vector<size_t> numOutChannels = {2};
+const std::vector<ngraph::op::PadType> pad_types = {ngraph::op::PadType::EXPLICIT};
+const auto inputShapes = std::vector<size_t>({1, 4, 10, 10});
 
 const auto groupConv2DParams = ::testing::Combine(::testing::ValuesIn(kernels),
                                                   ::testing::ValuesIn(strides),
@@ -36,11 +31,10 @@ const auto groupConv2DParams = ::testing::Combine(::testing::ValuesIn(kernels),
                                                   ::testing::ValuesIn(padEnds),
                                                   ::testing::ValuesIn(dilations),
                                                   ::testing::ValuesIn(numOutChannels),
-                                                  ::testing::ValuesIn(numGroups),
                                                   ::testing::ValuesIn(pad_types));
 
-INSTANTIATE_TEST_CASE_P(smoke_GroupConvolutionCUDA2D_Run,
-                        GroupConvolutionCUDALayerTest,
+INSTANTIATE_TEST_CASE_P(smoke_ConvolutionAsymPadCUDA2D_Run,
+                        ConvolutionAsymPadCUDALayerTest,
                         ::testing::Combine(groupConv2DParams,
                                            ::testing::ValuesIn(precisions),
                                            ::testing::Values(InferenceEngine::Precision::UNSPECIFIED),
@@ -49,6 +43,6 @@ INSTANTIATE_TEST_CASE_P(smoke_GroupConvolutionCUDA2D_Run,
                                            ::testing::Values(InferenceEngine::Layout::ANY),
                                            ::testing::Values(inputShapes),
                                            ::testing::Values(CommonTestUtils::DEVICE_CUDA)),
-                        GroupConvolutionCUDALayerTest::getTestCaseName);
+                        ConvolutionAsymPadCUDALayerTest::getTestCaseName);
 }  // namespace
 }  // namespace CUDALayerTestsDefinitions
