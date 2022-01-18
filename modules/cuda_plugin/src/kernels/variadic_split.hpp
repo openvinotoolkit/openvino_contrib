e86a86613e91574469b0ed3e607f624cd0ff6eeb
Denis Kotov <denys.kotov@altran.com>
2022-01-18 16:06:28 +0200

    Hotfix: Fix build of develop after 2 simultaneous merge PRs
    Added SECURITY.md
    
    Replaced 2 separate GetTensorByteSize methods with one template one
    
    Added VariadicSplitOp
    
    Tests for VariadicSplitOp
    
    Removed mutable keyword for TensorIterator as it is not safe in multi-threaded context

---
+++ modules/cuda_plugin/src/kernels/variadic_split.hpp
@@ -0,0 +1,49 @@
+// Copyright (C) 2018-2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include "cuda_type_traits.hpp"
+#include "error.hpp"
+
+namespace CUDAPlugin {
+namespace kernel {
+
+class VariadicSplit {
+public:
+    VariadicSplit(Type_t element_type,
+                  size_t num_all_chunks,
+                  size_t axis_split_step_size,
+                  size_t orig_axis_size,
+                  unsigned num_blocks,
+                  unsigned threads_per_block);
+    VariadicSplit(VariadicSplit &&) = default;
+    VariadicSplit &operator=(VariadicSplit &&) = default;
+
+    void operator()(cudaStream_t stream,
+                    const void *src,
+                    void **dst,
+                    const void *splitIdxs,
+                    const void *axisSizes,
+                    const void *axisOffsetSizes) const;
+
+private:
+    template <typename T>
+    void call(cudaStream_t stream,
+              const void *src,
+              void **dst,
+              const void *splitIdxs,
+              const void *axisSizes,
+              const void *axisOffsetSizes) const;
+
+    Type_t element_type_{};
+    size_t num_all_chunks_{};
+    size_t axis_split_step_size_{};
+    size_t orig_axis_size_{};
+    unsigned num_blocks_{};
+    unsigned threads_per_block_{};
+};
+
+}  // namespace kernel
+}  // namespace CUDAPlugin
