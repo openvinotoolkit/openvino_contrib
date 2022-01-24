2e24cde7a5cd8a39236547ca90af5db8ad7c9d7f
Anton Pankratov <anton.pankratov@intel.com>
2022-01-24 17:14:09 +0300

    Used any to pass config values (#262)

--- modules/arm_plugin/tests/functional/shared_tests_instances/behavior/ov_plugin/core_integration.cpp
+++ modules/arm_plugin/tests/functional/shared_tests_instances/behavior/ov_plugin/core_integration.cpp
@@ -77,20 +77,6 @@ INSTANTIATE_TEST_SUITE_P(
         smoke_OVClassGetConfigTest, OVClassGetConfigTest,
         ::testing::Values("CPU"));
 
-//////////////////////////////////////////////////////////////////////////////////////////
-
-TEST(OVClassBasicTest, smoke_SetConfigAfterCreatedThrow) {
-    ov::runtime::Core ie;
-    std::string value = {};
-
-    ASSERT_NO_THROW(ie.set_config({{KEY_CPU_THREADS_NUM, "1"}}, "CPU"));
-    ASSERT_NO_THROW(value = ie.get_config("CPU", KEY_CPU_THREADS_NUM).as<std::string>());
-    ASSERT_EQ("1", value);
-
-    ASSERT_NO_THROW(ie.set_config({{KEY_CPU_THREADS_NUM, "4"}}, "CPU"));
-    ASSERT_NO_THROW(value = ie.get_config("CPU", KEY_CPU_THREADS_NUM).as<std::string>());
-    ASSERT_EQ("4", value);
-}
 
 // IE Class Query network
 
