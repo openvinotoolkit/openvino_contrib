954fb8c01b13c4d7af81820da645f7371d7adc61
vitaliy.mostovy <vitaliy.mostovy@altran.com>
2022-01-28 20:11:55 +0200

    Add Broadcast operation tests
    Add Broadcast operation implementation
    
    Add noop broadcast graph transformation

---
+++ modules/cuda_plugin/src/transformer/noop_broadcast_transformation.cpp
@@ -0,0 +1,41 @@
+// Copyright (C) 2022 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "noop_broadcast_transformation.hpp"
+
+#include <gsl/gsl_assert>
+#include <ngraph/op/broadcast.hpp>
+#include <ngraph/pattern/op/wrap_type.hpp>
+
+namespace ngraph::pass {
+
+NGRAPH_RTTI_DEFINITION(ngraph::pass::NoopBroadcastTransformation, "NoopBroadcastTransformation", 0);
+
+namespace {
+
+bool eliminate_noop_broadcast(ngraph::pattern::Matcher &m) {
+    auto node = std::dynamic_pointer_cast<ngraph::op::v3::Broadcast>(m.get_match_root());
+    Expects(node);
+
+    auto in_shape = node->get_input_shape(0);
+    auto out_shape = node->get_output_shape(0);
+    if (in_shape != out_shape) {
+        return false;
+    }
+
+    return ngraph::replace_output_update_name(node->output(0), node->input_value(0));
+}
+
+}  // namespace
+
+NoopBroadcastTransformation::NoopBroadcastTransformation() {
+    const auto op = ngraph::pattern::wrap_type<ngraph::op::v3::Broadcast>();
+    const auto m = std::make_shared<ngraph::pattern::Matcher>(op, "NoopBroadcastTransformation");
+
+    matcher_pass_callback callback = [](ngraph::pattern::Matcher &m) { return eliminate_noop_broadcast(m); };
+
+    register_matcher(m, callback);
+}
+
+}  // namespace ngraph::pass
