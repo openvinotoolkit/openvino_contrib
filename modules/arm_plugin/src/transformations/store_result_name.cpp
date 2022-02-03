cdc468ca4664baedea6e14166623b2eb0c8f38b9
Vitaly Tuzov <vitaly.tuzov@intel.com>
2022-02-03 14:33:00 +0300

    Migrated Arm plugin to ov::Model

--- modules/arm_plugin/src/transformations/store_result_name.cpp
+++ modules/arm_plugin/src/transformations/store_result_name.cpp
@@ -7,8 +7,8 @@
 #include "transformations/utils/utils.hpp"
 
 NGRAPH_RTTI_DEFINITION(ArmPlugin::pass::StoreResultName, "StoreResultName", 0);
-bool ArmPlugin::pass::StoreResultName::run_on_function(std::shared_ptr<ngraph::Function> f) {
-    for (auto&& node : f->get_results()) {
+bool ArmPlugin::pass::StoreResultName::run_on_function(std::shared_ptr<ov::Model> m) {
+    for (auto&& node : m->get_results()) {
         IE_ASSERT(node->inputs().size() == 1);
         auto input = node->input(0);
         auto sourceOutput = input.get_source_output();
