a8d6e41e3bd7304a0cdfa1933cb2443312c54661
Denis Kotov <denys.kotov@altran.com>
2022-02-08 12:49:30 +0200

    Added CUDA::Pair

---
+++ modules/cuda_plugin/src/cuda/stl/pair.cuh
@@ -0,0 +1,20 @@
+// Copyright (C) 2018-2022 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+namespace CUDA {
+
+template <typename K, typename T>
+struct Pair {
+    K first;
+    T second;
+};
+
+template <typename K, typename T, typename... TArgs>
+__host__ __device__ Pair<K, T> make_pair(K k, T t) {
+    return Pair<K, T>{k, t};
+}
+
+}  // namespace CUDA
