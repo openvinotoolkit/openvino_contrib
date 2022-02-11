5ca1f77d2bf9f311134047b0b82214dc0d4fc7d6
Anton Pankratov <anton.pankratov@intel.com>
2022-02-11 09:22:52 +0300

    Streams property renamed (#306)

--- modules/arm_plugin/tests/functional/shared_tests_instances/behavior/ov_executable_network/exec_network_base.cpp
+++ modules/arm_plugin/tests/functional/shared_tests_instances/behavior/ov_executable_network/exec_network_base.cpp
@@ -51,7 +51,7 @@ namespace {
 
     const std::vector<ov::AnyMap> configSetPrc = {
             {},
-            {ov::streams::num(ov::streams::AUTO)}
+            {ov::num_streams(ov::NumStreams::AUTO)}
     };
 
     const std::vector<ov::AnyMap> AutoConfigsSetPrc = {
@@ -60,6 +60,6 @@ namespace {
 
     const std::vector<ov::AnyMap> MultiConfigsSetPrc = {
             {ov::device::priorities(CommonTestUtils::DEVICE_CPU)},
-            {ov::device::priorities(CommonTestUtils::DEVICE_CPU), ov::streams::num(ov::streams::AUTO)}
+            {ov::device::priorities(CommonTestUtils::DEVICE_CPU), ov::num_streams(ov::NumStreams::AUTO)}
     };
 }  // namespace
