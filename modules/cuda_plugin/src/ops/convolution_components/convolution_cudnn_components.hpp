217fc449c944515f9a9f6ab3a599120f748453aa
Andrii Pavliuk <andrii.pavliuk@altran.com>
2022-02-24 00:42:33 +0200

    [FusedConvolution] Add FusedConvolutionCuDnnDecomposed to handle issue with cudnnConvolutionBiasActivationForward() and CUDNN_ACTIVATION_IDENTITY
    [FusedConvolution] Add test smoke_Convolution2DBiasAddActivation_Negative_CUDNN_CONVOLUTION_FWD_ALGO_FFT_TILING

--- modules/cuda_plugin/src/ops/convolution_components/convolution_cudnn_components.hpp
+++ modules/cuda_plugin/src/ops/convolution_components/convolution_cudnn_components.hpp
@@ -152,4 +152,8 @@ private:
     cudnnConvolutionBwdDataAlgoPerf_t algo_perf_;
 };
 
+std::shared_ptr<CUDA::DnnTensorDescriptor> MakeFusedAddDescriptor(const ngraph::Shape& shape,
+                                                                  ngraph::element::Type_t element_type);
+std::shared_ptr<CUDA::DnnActivationDescriptor> MakeFusedActivationDescriptor(nodes::ActivationMode mode);
+
 }  // namespace CUDAPlugin::Convolution::Details
