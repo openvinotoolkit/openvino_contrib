8534e2a964f584c6cbe8c080c46c9ae1ce6b894a
Irina Efode <irina.efode@intel.com>
2022-10-13 14:29:45 +0200

    dd

--- modules/nvidia_plugin/tests/functional/shared_tests_instances/set_device_name.cpp
+++ modules/nvidia_plugin/tests/functional/shared_tests_instances/set_device_name.cpp
@@ -2,17 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
+#include "set_device_name.hpp"
+
 #include <string>
 #include <stdexcept>
 
-#include "set_device_name.hpp"
-
 namespace ov {
 namespace test {
+
 void set_device_suffix(const std::string& suffix) {
     if (!suffix.empty()) {
         throw std::runtime_error("The suffix can't be used for NVIDIA GPU device!");
     }
 }
+
 }  // namespace test
 }  // namespace ov
