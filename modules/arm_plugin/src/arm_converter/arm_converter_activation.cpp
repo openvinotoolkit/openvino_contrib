bc06852ac8a6fccbd978017b7580561028df44f7
Alexander Nesterov <nesterov.alexander@outlook.com>
2022-11-28 15:42:31 +0100

    Add native GELU

--- modules/arm_plugin/src/arm_converter/arm_converter_activation.cpp
+++ modules/arm_plugin/src/arm_converter/arm_converter_activation.cpp
@@ -80,6 +80,11 @@ template<> Converter::Conversion::Ptr Converter::Convert(const opset::SoftPlus& 
     return ConvertActivation(node, info, this);
 }
 
+template<> Converter::Conversion::Ptr Converter::Convert(const opset::Gelu& node) {
+    arm_compute::ActivationLayerInfo info(arm_compute::ActivationLayerInfo::ActivationFunction::GELU);
+    return ConvertActivation(node, info, this);
+}
+
 template<> Converter::Conversion::Ptr Converter::Convert(const opset::Log& node) {
     return MakeConversion<arm_compute::NELogLayer>(node.input(0), node.output(0));
 }
@@ -90,15 +95,6 @@ template<> Converter::Conversion::Ptr Converter::Convert(const opset::HSigmoid& 
     };
     return CallSwitch(
         AP_WRAP(make, ngraph::runtime::reference::hsigmoid),
-        node.input(0), floatTypes);
-}
-
-template<> Converter::Conversion::Ptr Converter::Convert(const opset::Gelu& node) {
-    auto make = [&] (auto refFunction) {
-        return this->MakeConversion(refFunction, node.input(0), node.output(0), node.get_approximation_mode(), ngraph::shape_size(node.get_output_shape(0)));
-    };
-    return CallSwitch(
-        AP_WRAP(make, ngraph::runtime::reference::gelu),
         node.input(0), floatTypes);
 }
 
