70540b098c2fbe78e77ef59c6d218f443e21befa
Alexander Nesterov <nesterov.alexander@outlook.com>
2022-11-28 17:06:48 +0100

    TANH FP16 is work

--- modules/arm_plugin/src/transformations/convert_precision_fp16_to_fp32.cpp
+++ modules/arm_plugin/src/transformations/convert_precision_fp16_to_fp32.cpp
@@ -27,8 +27,7 @@ bool check_op_type(const std::shared_ptr<ov::Node>& op) {
     CHECK_TYPE(ArmPlugin::opset::Squeeze, op);
     CHECK_TYPE(ArmPlugin::opset::Unsqueeze, op);
     CHECK_TYPE(ArmPlugin::opset::Sigmoid, op);
-//    Failed native layer in ACL for FP16 precision 
-//    CHECK_TYPE(ArmPlugin::opset::Tanh, op);
+    CHECK_TYPE(ArmPlugin::opset::Tanh, op);
     CHECK_TYPE(ArmPlugin::opset::Relu, op);
     CHECK_TYPE(ArmPlugin::opset::PRelu, op);
     CHECK_TYPE(ArmPlugin::opset::Abs, op);
