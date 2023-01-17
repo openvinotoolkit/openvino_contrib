f13c086a14230c7af7c34f1b06299f199ff63bb2
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2023-01-17 18:45:14 +0100

    Use ov::op::util (#550)
    Co-authored-by: Nesterov Alexander <alexander.nesterov@intel.com>

--- modules/arm_plugin/src/transformations/convert_rnn_cell.cpp
+++ modules/arm_plugin/src/transformations/convert_rnn_cell.cpp
@@ -52,7 +52,7 @@ ArmPlugin::pass::ConvertRNNCell::ConvertRNNCell() {
             clamp = std::make_shared<opset::Clamp>(i_t, -clip, clip);
             ngraph::copy_runtime_info(rnn_cell, clamp);
         }
-        auto out = ngraph::op::util::activation(rnn_cell->get_activations()[0], clamp);
+        auto out = ov::op::util::activation(rnn_cell->get_activations()[0], clamp);
         out->set_friendly_name(rnn_cell->get_friendly_name());
         ngraph::copy_runtime_info(rnn_cell, {Xt_W, Ht_R, add, i_t, out});
         ngraph::replace_node(rnn_cell, out);
