bfb482e4534878fe56fc779391024770a93109c0
Yaroslav Torziuk <yaroslav.torziuk@capgemini.com>
2023-05-17 17:20:06 +0200

    Add CUDA Graph C++ wrapper tests

---
+++ modules/nvidia_plugin/tests/unit/cuda/wrappers/graph.cu
@@ -0,0 +1,17 @@
+// Copyright (C) 2018-2023 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "graph.cuh"
+
+__global__ void VecAdd(int* A, int* B, int* C, int N)
+{
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i < N)
+        C[i] = A[i] + B[i];
+}
+
+void enqueueVecAdd(const CUDA::Stream s, dim3 gridDim, dim3 blockDim,
+                   int* devA, int* devB, int* devC, int N) {
+    s.run(gridDim, blockDim, VecAdd, devA, devB, devC, N);
+}
