309ef695f751e6f5196e95d3351b553555902803
Nadezhda <nadezhda.ageeva@intel.com>
2023-05-30 17:02:38 +0000

    Update RemoveRedundantConvertTransformation

--- modules/nvidia_plugin/tests/unit/transformations/remove_redundant_convert_transformation.cpp
+++ modules/nvidia_plugin/tests/unit/transformations/remove_redundant_convert_transformation.cpp
@@ -4,72 +4,87 @@
 
 #include <gtest/gtest.h>
 
-#include <tuple>
-
-#include "transformer/remove_redundant_convert_transformation.hpp"
-
 #include "common_test_utils/ngraph_test_utils.hpp"
 #include "openvino/core/model.hpp"
+#include "openvino/op/parameter.hpp"
 #include "openvino/opsets/opset10.hpp"
 #include "openvino/pass/manager.hpp"
 #include "transformations/init_node_info.hpp"
 #include "transformations/utils/utils.hpp"
+#include "transformer/remove_redundant_convert_transformation.hpp"
 
-using namespace ov::opset10;
-
-using namespace ngraph;
+using namespace ov;
 using namespace std;
 
 TEST(remove_redundant_convert, remove_redundant_convert) {
-    std::shared_ptr<Function> f;
+    std::shared_ptr<Model> f;
     {
         Shape shape{};
-        auto input_fp32 = make_shared<op::Parameter>(element::f32, shape);
+        auto input_fp32 = make_shared<op::v0::Parameter>(element::f32, shape);
         auto convert_f32 = make_shared<op::v0::Convert>(input_fp32, element::f32);
-        f = make_shared<Function>(make_shared<op::v0::Abs>(convert_f32), ParameterVector{input_fp32});
+        f = make_shared<Model>(make_shared<op::v0::Abs>(convert_f32), ParameterVector{input_fp32});
     }
 
     pass::Manager pass_manager;
-    pass_manager.register_pass<ov::pass::InitNodeInfo>();
-    pass_manager.register_pass<ov::nvidia_gpu::pass::RemoveRedundantConvertTransformation>();
+    pass_manager.register_pass<pass::InitNodeInfo>();
+    pass_manager.register_pass<nvidia_gpu::pass::RemoveRedundantConvertTransformation>();
     pass_manager.run_passes(f);
 
     ASSERT_EQ(count_ops_of_type<op::v0::Convert>(f), 0);
 }
 
 TEST(remove_redundant_convert, remove_symmetric_converts) {
-    std::shared_ptr<Function> f;
+    std::shared_ptr<Model> f;
     {
         Shape shape{};
-        auto input_fp32 = make_shared<op::Parameter>(element::f32, shape);
+        auto input_fp32 = make_shared<op::v0::Parameter>(element::f32, shape);
         auto convert_f16 = make_shared<op::v0::Convert>(input_fp32, element::f16);
         auto convert_f32 = make_shared<op::v0::Convert>(convert_f16, element::f32);
-        f = make_shared<Function>(make_shared<op::v0::Abs>(convert_f32), ParameterVector{input_fp32});
+        f = make_shared<Model>(make_shared<op::v0::Abs>(convert_f32), ParameterVector{input_fp32});
     }
 
     pass::Manager pass_manager;
-    pass_manager.register_pass<ov::pass::InitNodeInfo>();
-    pass_manager.register_pass<ov::nvidia_gpu::pass::RemoveRedundantConvertTransformation>();
+    pass_manager.register_pass<pass::InitNodeInfo>();
+    pass_manager.register_pass<nvidia_gpu::pass::RemoveRedundantConvertTransformation>();
     pass_manager.run_passes(f);
 
     ASSERT_EQ(count_ops_of_type<op::v0::Convert>(f), 0);
 }
 
 TEST(remove_redundant_convert, remove_several_converts) {
-    std::shared_ptr<Function> f;
+    std::shared_ptr<Model> f;
     {
         Shape shape{};
-        auto input_fp32 = make_shared<op::Parameter>(element::f32, shape);
+        auto input_fp32 = make_shared<op::v0::Parameter>(element::f32, shape);
         auto convert_f16 = make_shared<op::v0::Convert>(input_fp32, element::f16);
         auto convert_f64 = make_shared<op::v0::Convert>(convert_f16, element::f64);
         auto convert_f32 = make_shared<op::v0::Convert>(convert_f64, element::f32);
-        f = make_shared<Function>(make_shared<op::v0::Abs>(convert_f32), ParameterVector{input_fp32});
+        f = make_shared<Model>(make_shared<op::v0::Abs>(convert_f32), ParameterVector{input_fp32});
     }
 
     pass::Manager pass_manager;
-    pass_manager.register_pass<ov::pass::InitNodeInfo>();
-    pass_manager.register_pass<ov::nvidia_gpu::pass::RemoveRedundantConvertTransformation>();
+    pass_manager.register_pass<pass::InitNodeInfo>();
+    pass_manager.register_pass<nvidia_gpu::pass::RemoveRedundantConvertTransformation>();
     pass_manager.run_passes(f);
 
     ASSERT_EQ(count_ops_of_type<op::v0::Convert>(f), 0);
+}
+
+TEST(remove_redundant_convert, merge_converts_into_one) {
+    std::shared_ptr<Model> f;
+    {
+        Shape shape{};
+        auto input_fp32 = make_shared<op::v0::Parameter>(element::f32, shape);
+        auto convert_f16 = make_shared<op::v0::Convert>(input_fp32, element::f16);
+        auto convert_f64 = make_shared<op::v0::Convert>(convert_f16, element::f64);
+        f = make_shared<Model>(make_shared<op::v0::Abs>(convert_f64), ParameterVector{input_fp32});
+    }
+
+    pass::Manager pass_manager;
+    pass_manager.register_pass<pass::InitNodeInfo>();
+    pass_manager.register_pass<nvidia_gpu::pass::RemoveRedundantConvertTransformation>();
+    pass_manager.run_passes(f);
+
+    ASSERT_EQ(count_ops_of_type<op::v0::Convert>(f), 1);
+    ASSERT_EQ(f->get_results()[0]->input(0).get_element_type(), element::f64);
 }
