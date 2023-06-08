206422244f510fe868ed868134a42b7619c52c65
Alexander <kozzzloff@list.ru>
2023-06-08 18:13:17 +0400

    Fixed setup.pu issue

--- modules/token_merging/setup.py
+++ modules/token_merging/setup.py
@@ -11,7 +11,7 @@ setup(
     url="https://github.com/openvinotoolkit/openvino_contrib/tree/master/modules/token_merging",
     description="Token Merging for OpenVINO",
     install_requires=["torch==1.13.1+cpu", "torchvision==0.14.1+cpu"],
-    dependency_links=["https://download.pytorch.org/whl/cpu"]
+    dependency_links=["https://download.pytorch.org/whl/cpu"],
     extras_require=EXTRAS_REQUIRE,
     packages=find_packages(exclude=("examples", "build")),
     license = 'Apache 2.0',
