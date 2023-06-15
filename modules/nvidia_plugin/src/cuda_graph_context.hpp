cb0e22777fd43d0b8cc1c34fe0ae1ce5b2e85da1
Yaroslav Torziuk <yaroslav.torziuk@capgemini.com>
2023-06-15 19:14:27 +0200

    Add result and parameter nodes info in CudaGraphContext

--- modules/nvidia_plugin/src/cuda_graph_context.hpp
+++ modules/nvidia_plugin/src/cuda_graph_context.hpp
@@ -5,12 +5,16 @@
 #pragma once
 
 #include <cuda/graph.hpp>
+#include <map>
+#include <string>
 
 namespace ov {
 namespace nvidia_gpu {
 
 struct CudaGraphContext {
     std::optional<CUDA::GraphExec> graphExec_{};
+    std::map<std::string, CUDA::UploadNode> parameterNodes;
+    std::map<std::string, CUDA::DownloadNode> resultNodes;
 };
 
 } // namespace nvidia_gpu
