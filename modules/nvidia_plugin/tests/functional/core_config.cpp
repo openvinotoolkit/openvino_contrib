6fdd3597b3360ca9c4d5559098af3c13a481af0a
Irina Efode <irina.efode@intel.com>
2023-06-24 01:07:22 +0400

    Add core_configuration for SubgraphBaseTest -> PR17547 (#660)
    * Add core_configuration for SubgraphBaseTest -> PR17547
    
    * Add config as for GPU
    
    * use local constants

--- modules/nvidia_plugin/tests/functional/core_config.cpp
+++ modules/nvidia_plugin/tests/functional/core_config.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "functional_test_utils/core_config.hpp"
+#include "shared_test_classes/base/ov_subgraph.hpp"
+
 #include "cuda_test_constants.hpp"
 
 void CoreConfiguration(LayerTestsUtils::LayerTestsCommon* test) {
@@ -14,8 +16,25 @@ void CoreConfiguration(LayerTestsUtils::LayerTestsCommon* test) {
             break;
         }
     }
-
     // Set inference_precision hint to run fp32 model in fp32 runtime precision as default plugin execution precision may vary
     std::map<std::string, std::string> config = {{ov::hint::inference_precision.name(), hint.get_type_name()}};
     core->SetConfig(config, CommonTestUtils::DEVICE_NVIDIA);
 }
+
+namespace ov {
+namespace test {
+
+void core_configuration(ov::test::SubgraphBaseTest* test) {
+    ov::element::Type hint = ov::element::f32;
+    for (auto& param : test->function->get_parameters()) {
+        if (param->get_output_element_type(0) == ov::element::f16) {
+            hint = ov::element::f16;
+            break;
+        }
+    }
+    // Set inference_precision hint to run fp32 model in fp32 runtime precision as default plugin execution precision may vary
+    test->core->set_property(CommonTestUtils::DEVICE_NVIDIA, {{ov::hint::inference_precision.name(), hint.get_type_name()}});
+}
+
+}  // namespace test
+}  // namespace ov
