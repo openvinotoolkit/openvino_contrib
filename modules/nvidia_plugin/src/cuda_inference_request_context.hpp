590db8005ab5e3951af481314234aedf6ffe8958
Andrii Pavliuk <andrii.pavliuk@capgemini.com>
2023-08-25 17:26:28 +0300

    [NVIDIA] Extract inputs/outputs to TensorMappingContext

--- modules/nvidia_plugin/src/cuda_inference_request_context.hpp
+++ modules/nvidia_plugin/src/cuda_inference_request_context.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2018-2021 Intel Corporation
+// Copyright (C) 2018-2023 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 //
 
@@ -7,6 +7,7 @@
 #include <memory_manager/cuda_device_mem_block.hpp>
 
 #include "cancellation_token.hpp"
+#include "cuda_tensor_mapping_context.hpp"
 #include "cuda_thread_context.hpp"
 #include "cuda_graph_context.hpp"
 
@@ -35,12 +36,10 @@ public:
         : threadContext{threadContext},
           token{token},
           profiler{profiler},
-          blob_inputs{inputs},
-          inputs_mapping{inputMapping},
-          blob_outputs{outputs},
-          outputs_mapping{outputMapping},
+          tensor_mapping_context_{inputs, inputMapping, outputs, outputMapping},
           cuda_graph_context_{cudaGraphContext},
           is_benchmark_mode_{isBenchmarkMode} {}
+
     // don't allow storing references to temporary
     template <typename... Args>
     InferenceRequestContext(std::vector<std::shared_ptr<ov::Tensor>>&& inputs,
@@ -48,40 +47,18 @@ public:
                             std::vector<std::shared_ptr<ov::Tensor>>&& outputs,
                             std::map<std::string, std::size_t>&& outputMapping,
                             Args... args) = delete;
+
     InferenceRequestContext(std::vector<std::shared_ptr<ov::Tensor>>&& inputs,
                             std::map<std::string, std::size_t>&& inputMapping,
                             std::vector<std::shared_ptr<ov::Tensor>>&& outputs,
                             std::map<std::string, std::size_t>&& outputMapping,
                             const ThreadContext& threadContext) = delete;
 
-    /**
-     * @brief get_input_tensor(name) returns an tensor blob with the given name
-     */
-    std::shared_ptr<ov::Tensor> get_input_tensor(const std::string& input_name) const {
-        return blob_inputs.at(inputs_mapping.at(input_name));
-    }
-    /**
-     * @brief get_output_tensor(name) returns an output tensor with the given name
-     */
-    std::shared_ptr<ov::Tensor> get_output_tensor(const std::string& output_name) const {
-        return blob_outputs.at(outputs_mapping.at(output_name));
-    }
-    /**
-     * @brief has_input_tensor(name) returns true if it contains an input tensor with the given name
-     */
-    bool has_input_tensor(const std::string& input_name) const noexcept {
-        return inputs_mapping.find(input_name) != inputs_mapping.end();
-    }
-    /**
-     * @brief has_output_tensor(name) returns true if contains an output tensor with the given name
-     */
-    bool has_output_tensor(const std::string& output_name) const noexcept {
-        return outputs_mapping.find(output_name) != outputs_mapping.end();
-    }
     const ThreadContext& getThreadContext() const noexcept { return threadContext; }
     [[nodiscard]] ov::nvidia_gpu::CancellationToken& getCancellationToken() const noexcept { return token; }
     [[nodiscard]] Profiler& getProfiler() const noexcept { return profiler; }
     [[nodiscard]] bool isBenchmarkMode() const noexcept { return is_benchmark_mode_; }
+    [[nodiscard]] const TensorMappingContext& getTensorMappingContext() const { return tensor_mapping_context_; }
     [[nodiscard]] const CudaGraphContext& getCudaGraphContext() const { return cuda_graph_context_; }
     [[nodiscard]] CudaGraphContext& getCudaGraphContext() { return cuda_graph_context_; }
 
@@ -89,10 +66,7 @@ private:
     const ThreadContext& threadContext;
     CancellationToken& token;
     Profiler& profiler;
-    const std::vector<std::shared_ptr<ov::Tensor>>& blob_inputs;
-    const std::map<std::string, std::size_t>& inputs_mapping;
-    const std::vector<std::shared_ptr<ov::Tensor>>& blob_outputs;
-    const std::map<std::string, std::size_t>& outputs_mapping;
+    const TensorMappingContext tensor_mapping_context_;
     CudaGraphContext& cuda_graph_context_;
     bool is_benchmark_mode_;
 };
