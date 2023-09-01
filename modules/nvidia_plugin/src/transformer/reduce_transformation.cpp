0def49ea01c46816b00120dfa058dbe8cb324b9f
Nadezhda <nadezhda.ageeva@intel.com>
2023-09-01 14:20:57 +0000

    Update ReduceTransformation

--- modules/nvidia_plugin/src/transformer/reduce_transformation.cpp
+++ modules/nvidia_plugin/src/transformer/reduce_transformation.cpp
@@ -6,6 +6,7 @@
 
 #include "exec_graph_info.hpp"
 #include "openvino/core/rt_info.hpp"
+#include "openvino/pass/manager.hpp"
 #include "openvino/pass/pattern/op/wrap_type.hpp"
 #include "openvino/op/reduce_max.hpp"
 #include "openvino/op/reduce_mean.hpp"
@@ -13,6 +14,7 @@
 #include "openvino/op/reduce_prod.hpp"
 #include "openvino/op/reduce_sum.hpp"
 #include "openvino/op/reshape.hpp"
+#include "transformations/op_conversions/convert_reduce_to_reshape.hpp"
 #include "reduce_transformation.hpp"
 
 using namespace ov::pass::pattern;
@@ -37,65 +39,95 @@ bool is_reduce_to_be_transformed(const ov::Output<ov::Node>& output) {
     return input_shape == output_shape;
 }
 
+template <class T>
 bool transform_reduce(Matcher &m) {
     auto reduce = std::dynamic_pointer_cast<ov::op::util::ArithmeticReductionKeepDims>(m.get_match_root());
     const ov::Shape input_shape = reduce->input(0).get_shape();
     const ov::Shape output_shape = reduce->output(0).get_shape();
     auto consumers = reduce->get_output_target_inputs(0);
 
-    std::shared_ptr<ov::Node> new_reduce;
-    if (ov::as_type_ptr<ov::op::v1::ReduceMax>(reduce)) {
-        new_reduce = std::make_shared<ov::op::v1::ReduceMax>(reduce->input_value(0), reduce->input_value(1), true);
-    } else if (ov::as_type_ptr<ov::op::v1::ReduceMean>(reduce)) {
-        new_reduce = std::make_shared<ov::op::v1::ReduceMean>(reduce->input_value(0), reduce->input_value(1), true);
-    } else if (ov::as_type_ptr<ov::op::v1::ReduceMin>(reduce)) {
-        new_reduce = std::make_shared<ov::op::v1::ReduceMin>(reduce->input_value(0), reduce->input_value(1), true);
-    } else if (ov::as_type_ptr<ov::op::v1::ReduceProd>(reduce)) {
-        new_reduce = std::make_shared<ov::op::v1::ReduceProd>(reduce->input_value(0), reduce->input_value(1), true);
-    } else if (ov::as_type_ptr<ov::op::v1::ReduceSum>(reduce)) {
-        new_reduce = std::make_shared<ov::op::v1::ReduceSum>(reduce->input_value(0), reduce->input_value(1), true);
-    } else {
-        return false;
-    }
+    auto new_reduce = std::make_shared<T>(reduce->input_value(0), reduce->input_value(1), true);
     new_reduce->set_friendly_name(reduce->get_friendly_name());
 
     ov::NodeVector new_ops;
     const ov::Shape new_output_shape = new_reduce->output(0).get_shape();
-    if (input_shape != new_output_shape) {
-        auto reshape_const = std::make_shared<ov::op::v0::Constant>(element::i32, Shape{output_shape.size()}, output_shape);
-        auto reshape = std::make_shared<ov::op::v1::Reshape>(new_reduce, reshape_const, false);
-        for (auto consumer : consumers) {
-            consumer.replace_source_output(reshape);
-        }
-        new_ops = {new_reduce, reshape_const, reshape};
-    } else {
-        auto reshape_const = std::make_shared<ov::op::v0::Constant>(element::i32, Shape{output_shape.size()}, output_shape);
-        auto reshape = std::make_shared<ov::op::v1::Reshape>(reduce->input_value(0), reshape_const, false);
-        for (auto consumer : consumers) {
-            consumer.replace_source_output(reshape);
-        }
-        new_ops = {reshape_const, reshape};
+    auto reshape_const = std::make_shared<ov::op::v0::Constant>(element::i64, Shape{output_shape.size()}, output_shape);
+    auto reshape = std::make_shared<ov::op::v1::Reshape>(new_reduce, reshape_const, false);
+    for (auto consumer : consumers) {
+        consumer.replace_source_output(reshape);
     }
+    new_ops = {new_reduce, reshape_const, reshape};
+
     ov::copy_runtime_info(reduce, new_ops);
-    for (auto& new_op : new_ops) {
-        new_op->get_rt_info()[ExecGraphInfoSerialization::ORIGINAL_NAMES] = reduce->get_friendly_name();
-    }
     return true;
 }
 } // namespace
 
-ReduceTransformation::ReduceTransformation() {
-    MATCHER_SCOPE(ReduceTransformation);
-    auto reduce = wrap_type<ov::op::v1::ReduceMax,
-                            ov::op::v1::ReduceMean,
-                            ov::op::v1::ReduceMin,
-                            ov::op::v1::ReduceProd,
-                            ov::op::v1::ReduceSum>(is_reduce_to_be_transformed);
+ReduceMaxTransformation::ReduceMaxTransformation() {
+    MATCHER_SCOPE(ReduceMaxTransformation);
+    auto reduce = wrap_type<ov::op::v1::ReduceMax>(is_reduce_to_be_transformed);
     matcher_pass_callback callback = [](Matcher &m) {
-        return transform_reduce(m);
+        return transform_reduce<ov::op::v1::ReduceMax>(m);
         };
     auto m = std::make_shared<Matcher>(reduce, matcher_name);
     register_matcher(m, callback);
+}
+
+ReduceMeanTransformation::ReduceMeanTransformation() {
+    MATCHER_SCOPE(ReduceMeanTransformation);
+    auto reduce = wrap_type<ov::op::v1::ReduceMean>(is_reduce_to_be_transformed);
+    matcher_pass_callback callback = [](Matcher &m) {
+        return transform_reduce<ov::op::v1::ReduceMean>(m);
+        };
+    auto m = std::make_shared<Matcher>(reduce, matcher_name);
+    register_matcher(m, callback);
+}
+
+ReduceMinTransformation::ReduceMinTransformation() {
+    MATCHER_SCOPE(ReduceMinTransformation);
+    auto reduce = wrap_type<ov::op::v1::ReduceMin>(is_reduce_to_be_transformed);
+    matcher_pass_callback callback = [](Matcher &m) {
+        return transform_reduce<ov::op::v1::ReduceMin>(m);
+        };
+    auto m = std::make_shared<Matcher>(reduce, matcher_name);
+    register_matcher(m, callback);
+}
+
+ReduceProdTransformation::ReduceProdTransformation() {
+    MATCHER_SCOPE(ReduceProdTransformation);
+    auto reduce = wrap_type<ov::op::v1::ReduceProd>(is_reduce_to_be_transformed);
+    matcher_pass_callback callback = [](Matcher &m) {
+        return transform_reduce<ov::op::v1::ReduceProd>(m);
+        };
+    auto m = std::make_shared<Matcher>(reduce, matcher_name);
+    register_matcher(m, callback);
+}
+
+ReduceSumTransformation::ReduceSumTransformation() {
+    MATCHER_SCOPE(ReduceSumTransformation);
+    auto reduce = wrap_type<ov::op::v1::ReduceSum>(is_reduce_to_be_transformed);
+    matcher_pass_callback callback = [](Matcher &m) {
+        return transform_reduce<ov::op::v1::ReduceSum>(m);
+        };
+    auto m = std::make_shared<Matcher>(reduce, matcher_name);
+    register_matcher(m, callback);
+}
+
+bool ov::nvidia_gpu::pass::ReduceTransformation::run_on_model(const std::shared_ptr<ov::Model>& m) {
+    RUN_ON_FUNCTION_SCOPE(ReduceTransformation);
+    ov::pass::Manager manager;
+    manager.register_pass<ov::pass::ConvertReduceToReshape>();
+
+    auto reduce_transformations = manager.register_pass<ov::pass::GraphRewrite>();
+    ADD_MATCHER(reduce_transformations, ReduceMaxTransformation)
+    ADD_MATCHER(reduce_transformations, ReduceMeanTransformation)
+    ADD_MATCHER(reduce_transformations, ReduceMinTransformation)
+    ADD_MATCHER(reduce_transformations, ReduceProdTransformation)
+    ADD_MATCHER(reduce_transformations, ReduceSumTransformation)
+    reduce_transformations->set_name("ov::nvidia_gpu::reduce_transformations");
+
+    manager.run_passes(m);
+    return false;
 }
 
 }  // namespace ov::nvidia_gpu::pass
--- modules/nvidia_plugin/src/transformer/reduce_transformation.hpp
+++ modules/nvidia_plugin/src/transformer/reduce_transformation.hpp
@@ -8,10 +8,40 @@
 
 namespace ov::nvidia_gpu::pass {
 
-class ReduceTransformation : public ov::pass::MatcherPass {
+class ReduceTransformation : public ov::pass::ModelPass {
 public:
     OPENVINO_RTTI("ReduceTransformation", "0");
-    ReduceTransformation();
+    bool run_on_model(const std::shared_ptr<ov::Model>& m) override;
+};
+
+class ReduceMaxTransformation : public ov::pass::MatcherPass {
+public:
+    OPENVINO_RTTI("ReduceMaxTransformation", "0");
+    ReduceMaxTransformation();
+};
+
+class ReduceMeanTransformation : public ov::pass::MatcherPass {
+public:
+    OPENVINO_RTTI("ReduceMeanTransformation", "0");
+    ReduceMeanTransformation();
+};
+
+class ReduceMinTransformation : public ov::pass::MatcherPass {
+public:
+    OPENVINO_RTTI("ReduceMinTransformation", "0");
+    ReduceMinTransformation();
+};
+
+class ReduceProdTransformation : public ov::pass::MatcherPass {
+public:
+    OPENVINO_RTTI("ReduceProdTransformation", "0");
+    ReduceProdTransformation();
+};
+
+class ReduceSumTransformation : public ov::pass::MatcherPass {
+public:
+    OPENVINO_RTTI("ReduceSumTransformation", "0");
+    ReduceSumTransformation();
 };
 
 }  // namespace ov::nvidia_gpu::pass
