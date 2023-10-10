f69117598f683347658264668f37eedf8b9503b1
Nadezhda Ageeva <nadezhda.ageeva@intel.com>
2023-10-10 16:13:45 +0400

    [NVIDIA] Move Convert, ConvertColorI420, ConvertColorNV12 to API2.0 (#736)

--- modules/nvidia_plugin/tests/functional/shared_tests_instances/single_layer_tests/convert_color_i420.cpp
+++ modules/nvidia_plugin/tests/functional/shared_tests_instances/single_layer_tests/convert_color_i420.cpp
@@ -2,104 +2,23 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 
-#include "single_layer_tests/convert_color_i420.hpp"
+#include "single_op_tests/convert_color_i420.hpp"
+#include "convert_color_common.hpp"
 
-#include <cuda_test_constants.hpp>
+#include "cuda_test_constants.hpp"
 #include <vector>
-
-using namespace LayerTestsDefinitions;
 
 namespace {
 
-class ConvertColorI420CUDALayerTest : public ConvertColorI420LayerTest {
-private:
-    void SetUp() override {
-        ov::Shape inputShape;
-        ov::element::Type ngPrc;
-        bool conversionToRGB, singlePlane;
-        abs_threshold = 1.0f;  // I420 conversion can use various algorithms, thus some absolute deviation is allowed
-        threshold = 1.f;       // Ignore relative comparison for I420 convert (allow 100% relative deviation)
-        std::tie(inputShape, ngPrc, conversionToRGB, singlePlane, targetDevice) = GetParam();
-        if (singlePlane) {
-            inputShape[1] = inputShape[1] * 3 / 2;
-            auto param = std::make_shared<ov::op::v0::Parameter>(ngPrc, inputShape);
-            std::shared_ptr<ov::Node> convert_color;
-            if (conversionToRGB) {
-                convert_color = std::make_shared<ov::op::v8::I420toRGB>(param);
-            } else {
-                convert_color = std::make_shared<ov::op::v8::I420toRGB>(param);
-            }
-            function = std::make_shared<ov::Model>(
-                std::make_shared<ov::op::v0::Result>(convert_color), ov::ParameterVector{param}, "ConvertColorI420");
-        } else {
-            auto uvShape = ov::Shape{inputShape[0], inputShape[1] / 2, inputShape[2] / 2, 1};
-            auto param_y = std::make_shared<ov::op::v0::Parameter>(ngPrc, inputShape);
-            auto param_u = std::make_shared<ov::op::v0::Parameter>(ngPrc, uvShape);
-            auto param_v = std::make_shared<ov::op::v0::Parameter>(ngPrc, uvShape);
-            std::shared_ptr<ov::Node> convert_color;
-            if (conversionToRGB) {
-                convert_color = std::make_shared<ov::op::v8::I420toRGB>(param_y, param_u, param_v);
-            } else {
-                convert_color = std::make_shared<ov::op::v8::I420toBGR>(param_y, param_u, param_v);
-            }
-            function = std::make_shared<ov::Model>(std::make_shared<ov::op::v0::Result>(convert_color),
-                                                   ov::ParameterVector{param_y, param_u, param_v},
-                                                   "ConvertColorI420");
-        }
-    }
+using namespace ov::test;
+using namespace ov::test::nvidia_gpu;
+using namespace ov::test::utils;
 
-    InferenceEngine::Blob::Ptr GenerateInput(const InferenceEngine::InputInfo& info) const override {
-        return FuncTestUtils::createAndFillBlob(info.getTensorDesc(), 255);
-    }
-};
+class ConvertColorI420CUDALayerTest : public ConvertColorCUDALayerTest, public ConvertColorI420LayerTest {};
 
-TEST_P(ConvertColorI420CUDALayerTest, CompareWithRefs) { Run(); }
+TEST_P(ConvertColorI420CUDALayerTest, Inference) { run(); }
 
-class ConvertColorI420CUDAAccuracyTest : public ConvertColorI420AccuracyTest {
-private:
-    void SetUp() override {
-        ov::Shape inputShape;
-        ov::element::Type ngPrc;
-        bool conversionToRGB, singlePlane;
-        abs_threshold = 1.0f;  // I420 conversion can use various algorithms, thus some absolute deviation is allowed
-        threshold = 1.f;       // Ignore relative comparison for I420 convert (allow 100% relative deviation)
-        std::tie(inputShape, ngPrc, conversionToRGB, singlePlane, targetDevice) = GetParam();
-        if (singlePlane) {
-            inputShape[1] = inputShape[1] * 3 / 2;
-            auto param = std::make_shared<ov::op::v0::Parameter>(ngPrc, inputShape);
-            std::shared_ptr<ov::Node> convert_color;
-            if (conversionToRGB) {
-                convert_color = std::make_shared<ov::op::v8::I420toRGB>(param);
-            } else {
-                convert_color = std::make_shared<ov::op::v8::I420toRGB>(param);
-            }
-            function = std::make_shared<ov::Model>(
-                std::make_shared<ov::op::v0::Result>(convert_color), ov::ParameterVector{param}, "ConvertColorI420");
-        } else {
-            auto uvShape = ov::Shape{inputShape[0], inputShape[1] / 2, inputShape[2] / 2, 1};
-            auto param_y = std::make_shared<ov::op::v0::Parameter>(ngPrc, inputShape);
-            auto param_u = std::make_shared<ov::op::v0::Parameter>(ngPrc, uvShape);
-            auto param_v = std::make_shared<ov::op::v0::Parameter>(ngPrc, uvShape);
-            std::shared_ptr<ov::Node> convert_color;
-            if (conversionToRGB) {
-                convert_color = std::make_shared<ov::op::v8::I420toRGB>(param_y, param_u, param_v);
-            } else {
-                convert_color = std::make_shared<ov::op::v8::I420toBGR>(param_y, param_u, param_v);
-            }
-            function = std::make_shared<ov::Model>(std::make_shared<ov::op::v0::Result>(convert_color),
-                                                   ov::ParameterVector{param_y, param_u, param_v},
-                                                   "ConvertColorI420");
-        }
-    }
-
-    InferenceEngine::Blob::Ptr GenerateInput(const InferenceEngine::InputInfo& info) const override {
-        return FuncTestUtils::createAndFillBlob(info.getTensorDesc(), 255);
-    }
-};
-
-TEST_P(ConvertColorI420CUDAAccuracyTest, CompareWithRefs) { Run(); }
-
-const std::vector<ov::Shape> inShapes_nhwc = {
+const std::vector<ov::Shape> in_shapes_nhwc = {
     {1, 10, 10, 1},
     {1, 50, 10, 1},
     {1, 100, 10, 1},
@@ -112,43 +31,78 @@ const std::vector<ov::Shape> inShapes_nhwc = {
     {1, 96, 16, 1},
 };
 
-const std::vector<ov::element::Type> inTypes = {
+auto generate_input_static_shapes = [] (const std::vector<ov::Shape>& original_shapes, bool single_plane) {
+    std::vector<std::vector<ov::Shape>> result_shapes;
+    for (const auto& original_shape : original_shapes) {
+        std::vector<ov::Shape> one_result_shapes;
+        if (single_plane) {
+            auto shape = original_shape;
+            shape[1] = shape[1] * 3 / 2;
+            one_result_shapes.push_back(shape);
+        } else {
+            auto shape = original_shape;
+            one_result_shapes.push_back(shape);
+            auto uvShape = ov::Shape{shape[0], shape[1] / 2, shape[2] / 2, 1};
+            one_result_shapes.push_back(uvShape);
+            one_result_shapes.push_back(uvShape);
+        }
+        result_shapes.push_back(one_result_shapes);
+    }
+    return result_shapes;
+};
+
+auto in_shapes_single_plain_static     = generate_input_static_shapes(in_shapes_nhwc, true);
+auto in_shapes_not_single_plain_static = generate_input_static_shapes(in_shapes_nhwc, false);
+
+const std::vector<ov::element::Type> in_types = {
     ov::element::u8,
     ov::element::f32,
     ov::element::f16,
 };
 
-const auto testCase_values = ::testing::Combine(::testing::ValuesIn(inShapes_nhwc),
-                                                ::testing::ValuesIn(inTypes),
-                                                ::testing::Bool(),
-                                                ::testing::Bool(),
-                                                ::testing::Values(ov::test::utils::DEVICE_NVIDIA));
+const auto test_case_values_single_plain = ::testing::Combine(::testing::ValuesIn(static_shapes_to_test_representation(in_shapes_single_plain_static)),
+                                                              ::testing::ValuesIn(in_types),
+                                                              ::testing::Bool(),
+                                                              ::testing::Values(true),
+                                                              ::testing::Values(DEVICE_NVIDIA));
 
-INSTANTIATE_TEST_SUITE_P(smoke_TestsConvertColorI420,
+INSTANTIATE_TEST_SUITE_P(smoke_TestsConvertColorI420_single_plain,
                          ConvertColorI420CUDALayerTest,
-                         testCase_values,
+                         test_case_values_single_plain,
                          ConvertColorI420CUDALayerTest::getTestCaseName);
 
-const auto testCase_accuracy_values = ::testing::Combine(::testing::Values(ov::Shape{1, 96, 16, 1}),
-                                                         ::testing::Values(ov::element::u8),
-                                                         ::testing::Values(false),
-                                                         ::testing::Values(true),
-                                                         ::testing::Values(ov::test::utils::DEVICE_NVIDIA));
+const auto test_case_values_not_single_plain = ::testing::Combine(::testing::ValuesIn(static_shapes_to_test_representation(in_shapes_not_single_plain_static)),
+                                                                  ::testing::ValuesIn(in_types),
+                                                                  ::testing::Bool(),
+                                                                  ::testing::Values(false),
+                                                                  ::testing::Values(DEVICE_NVIDIA));
+
+INSTANTIATE_TEST_SUITE_P(smoke_TestsConvertColorI420_not_single_plain,
+                         ConvertColorI420CUDALayerTest,
+                         test_case_values_not_single_plain,
+                         ConvertColorI420CUDALayerTest::getTestCaseName);
+
+
+const auto test_case_accuracy_values = ::testing::Combine(::testing::Values(static_shapes_to_test_representation({ov::Shape{1, 96, 16, 1}})),
+                                                          ::testing::Values(ov::element::u8),
+                                                          ::testing::Values(false),
+                                                          ::testing::Values(true),
+                                                          ::testing::Values(DEVICE_NVIDIA));
 
 INSTANTIATE_TEST_SUITE_P(smoke_TestsConvertColorI420_acc,
-                         ConvertColorI420CUDAAccuracyTest,
-                         testCase_accuracy_values,
+                         ConvertColorI420CUDALayerTest,
+                         test_case_accuracy_values,
                          ConvertColorI420CUDALayerTest::getTestCaseName);
 
-const auto testCase_accuracy_values_nightly = ::testing::Combine(::testing::Values(ov::Shape{1, 65536, 256, 1}),
-                                                                 ::testing::Values(ov::element::u8),
-                                                                 ::testing::Values(false),
-                                                                 ::testing::Values(true),
-                                                                 ::testing::Values(ov::test::utils::DEVICE_NVIDIA));
+const auto test_case_accuracy_values_nightly = ::testing::Combine(::testing::Values(static_shapes_to_test_representation({ov::Shape{1, 65538, 256, 1}})),
+                                                                  ::testing::Values(ov::element::u8),
+                                                                  ::testing::Values(false),
+                                                                  ::testing::Values(true),
+                                                                  ::testing::Values(DEVICE_NVIDIA));
 
 INSTANTIATE_TEST_SUITE_P(nightly_TestsConvertColorI420_acc,
-                         ConvertColorI420CUDAAccuracyTest,
-                         testCase_accuracy_values_nightly,
+                         ConvertColorI420CUDALayerTest,
+                         test_case_accuracy_values_nightly,
                          ConvertColorI420CUDALayerTest::getTestCaseName);
 
 }  // namespace
