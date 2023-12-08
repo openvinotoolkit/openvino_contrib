bfe06649825992e9bc5de40c407c06658777b77e
Artur Paniukov <chgk1101@gmail.com>
2023-12-08 14:44:45 +0000

    Refactoring
    Del node_factory.py
    Move from os to Path
    Support -e install for _ext_libs_path
    Wrap Core.__init__

--- modules/custom_operations/user_ie_extensions/tokenizer/python/ov_tokenizer/__init__.py
+++ modules/custom_operations/user_ie_extensions/tokenizer/python/ov_tokenizer/__init__.py
@@ -1,48 +1,51 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2018-2023 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-
+import functools
 import os
 import sys
+import sysconfig
+from pathlib import Path
 
 import openvino
 from openvino.runtime.utils.node_factory import NodeFactory
 
 from .convert_tokenizer import convert_tokenizer
-from .node_factory import _extension_path, init_extension
 from .str_pack import pack_strings, unpack_strings
 from .utils import add_greedy_decoding, connect_models
 
 
+_extension_path = os.environ.get("OV_TOKENIZER_PREBUILD_EXTENSION_PATH")
 _ext_name = "user_ov_extensions"
 if _extension_path:
     # when the path to extension set manually
-    _ext_libs_path = os.path.dirname(_extension_path)
+    _ext_libs_path = Path(_extension_path).parent
 else:
     # python installation case
-    _ext_libs_path = os.path.join(os.path.dirname(__file__), "libs")
+    _ext_libs_path = Path(sysconfig.get_paths()["purelib"]) / __name__ / "libs"
 
 if sys.platform == "win32":
-    _ext_path = os.path.join(_ext_libs_path, f"{_ext_name}.dll")
-    if os.path.isdir(_ext_libs_path):
+    _ext_path = _ext_libs_path / f"{_ext_name}.dll"
+    if _ext_libs_path.is_dir():
         # On Windows, with Python >= 3.8, DLLs are no longer imported from the PATH.
-        os.add_dll_directory(os.path.abspath(_ext_libs_path))
+        os.add_dll_directory(str(_ext_libs_path.absolute()))
     else:
         sys.exit(f"Error: extention libriary path {_ext_libs_path} not found")
 elif sys.platform == "darwin":
-    _ext_path = os.path.join(_ext_libs_path, f"lib{_ext_name}.dylib")
+    _ext_path = _ext_libs_path / f"lib{_ext_name}.dylib"
 elif sys.platform == "linux":
-    _ext_path = os.path.join(_ext_libs_path, f"lib{_ext_name}.so")
+    _ext_path = _ext_libs_path / f"lib{_ext_name}.so"
 else:
-    sys.exit(f"Error: extention does not support platform {sys.platform}")
+    sys.exit(f"Error: extension does not support platform {sys.platform}")
 
 # patching openvino
 old_core_init = openvino.runtime.Core.__init__
 
 
-def new_core_init(self, *k, **kw):
-    old_core_init(self, *k, **kw)
-    self.add_extension(_ext_path)
+@functools.wraps(old_core_init)
+def new_core_init(self, *args, **kwargs):
+    old_core_init(self, *args, **kwargs)
+    self.add_extension(str(_ext_path))  # Core.add_extension doesn't support Path object
 
 
 openvino.runtime.Core.__init__ = new_core_init
