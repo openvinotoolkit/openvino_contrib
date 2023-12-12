23ba0a6f7d57b81aaad6467250219103ec64e1a7
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2023-12-12 09:48:26 +0100

    [Tokenizers] package metadata and minor fixes (#776)
    * wheel metadata
    
    * removed CUSTOM_OP_LIB
    
    * Revert "removed CUSTOM_OP_LIB"
    
    This reverts commit 208dcc06ec0053f05cf0819bec0250fcd8c87229.
    
    * added licenses
    
    * filtered source package
    
    * Update modules/custom_operations/user_ie_extensions/tokenizer/python/README.md
    
    Co-authored-by: Artur Paniukov <chgk1101@gmail.com>
    
    * set custom python executable
    
    * renamed ov_tokenizer to openvino_tokenizer
    
    * Update mac.yml
    
    Specify Python3_EXECUTABLE for OpenVINO
    
    * Apply suggestions from code review
    
    replaced `variables` with `env`
    
    * openvino_tokenizer -> openvino_tokenizers
    
    * included security.md
    
    * update package names in ci
    
    * Update mac.yml
    
    Fixed macOS
    
    * Apply suggestions from code review
    
    * Update .ci/azure/mac.yml
    
    ---------
    
    Co-authored-by: Artur Paniukov <chgk1101@gmail.com>
    Co-authored-by: Artur Paniukov <artur.paniukov@intel.com>
    Co-authored-by: Ilya Lavrenov <ilya.lavrenov@intel.com>

--- modules/custom_operations/user_ie_extensions/tokenizer/python/tests/tokenizers_test.py
+++ modules/custom_operations/user_ie_extensions/tokenizer/python/tests/tokenizers_test.py
@@ -9,7 +9,7 @@
 import numpy as np
 import pytest
 from openvino import Core
-from ov_tokenizer import (
+from openvino_tokenizers import (
     convert_tokenizer,
     pack_strings,
     unpack_strings,
