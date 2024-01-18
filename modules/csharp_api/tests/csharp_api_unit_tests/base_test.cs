d8cd17382d935765ed7d8e56e796209ed0f30b71
yanguojin <guojin_yjs@cumt.edu.cn>
2024-01-18 17:49:15 +0800

    Add unit tests.

---
+++ modules/csharp_api/tests/csharp_api_unit_tests/base_test.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenVinoSharp.Tests
+{
+    public class OVBaseTest
+    {
+        public class TestModelInfo
+        {
+            public string model_xml = "..\\..\\..\\..\\..\\tests\\test_data\\model\\yolov8\\yolov8s.xml";
+            public string model_bin = "..\\..\\..\\..\\..\\tests\\test_data\\model\\yolov8\\yolov8s.bin";
+            public string input_name = "images";
+            public string output_name = "output0";
+
+            public OvType input_type = new OvType(ElementType.F32);
+            public Shape input_shape = new Shape(new long[] { 1, 3, 640, 640 });
+
+            public OvType output_type = new OvType(ElementType.F16);
+            public Shape output_shape = new Shape(new long[] { 1, 84, 8400 });
+        }
+        TestModelInfo model_info = new TestModelInfo();
+
+        private string device = "CPU";
+        public string get_model_xml_file_name() 
+        {
+            if (!File.Exists(model_info.model_xml)) 
+            {
+                Assert.Fail();
+            }
+            return model_info.model_xml;
+        }
+        public string get_model_bin_file_name()
+        {
+            if (!File.Exists(model_info.model_bin))
+            {
+                Assert.Fail();
+            }
+            return model_info.model_bin;
+        }
+        public string get_device() 
+        {
+            return device;
+        }
+
+        public string model_input_name() 
+        {
+            return model_info.input_name;
+        }
+        public string model_output_name()
+        {
+            return model_info.output_name;
+        }
+
+        public Shape model_input_shape() 
+        {
+            return model_info.input_shape;
+        }
+
+        public OvType model_input_type()
+        {
+            return model_info.input_type;
+        }
+
+        public Shape model_output_shape()
+        {
+            return model_info.output_shape;
+        }
+
+        public OvType model_output_type()
+        {
+            return model_info.output_type;
+        }
+
+        public byte[] content_from_file(string file)
+        {
+            FileStream fs = new FileStream(get_model_bin_file_name(), FileMode.Open, FileAccess.Read);
+
+            long len = fs.Seek(0, SeekOrigin.End);
+
+
+            fs.Seek(0, SeekOrigin.Begin);
+
+            byte[] data = new byte[len + 1];
+
+            fs.Read(data, 0, (int)len);
+            return data;
+        }
+    }
+}
---
+++ modules/csharp_api/tests/csharp_api_unit_tests/preprocess/PrePostProcessorTests.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenVinoSharp.preprocess;
+using OpenVinoSharp.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenVinoSharp.preprocess.Tests
+{
+    [TestClass()]
+    public class PrePostProcessorTests : OVBaseTest
+    {
+        [TestMethod()]
+        public void PrePostProcessor_test()
+        {
+            var core = new Core();
+            Model model = core.read_model(get_model_xml_file_name());
+            Assert.IsTrue(model.Ptr != IntPtr.Zero);
+            PrePostProcessor processor = new PrePostProcessor(model);
+            processor.Dispose();
+            model.Dispose();
+        }
+
+        [TestMethod()]
+        public void Dispose_test()
+        {
+        }
+
+        [TestMethod()]
+        public void input_test()
+        {
+            var core = new Core();
+            Model model = core.read_model(get_model_xml_file_name());
+            Assert.IsTrue(model.Ptr != IntPtr.Zero);
+            PrePostProcessor processor = new PrePostProcessor(model);
+            InputInfo input = processor.input();
+            input.Dispose();
+            processor.Dispose();
+            model.Dispose(); 
+        }
+
+        [TestMethod()]
+        public void input_test1()
+        {
+            var core = new Core();
+            Model model = core.read_model(get_model_xml_file_name());
+            Assert.IsTrue(model.Ptr != IntPtr.Zero);
+            PrePostProcessor processor = new PrePostProcessor(model);
+            InputInfo input = processor.input(model_input_name());
+            input.Dispose();
+            processor.Dispose();
+            model.Dispose();
+        }
+
+        [TestMethod()]
+        public void input_test2()
+        {
+            var core = new Core();
+            Model model = core.read_model(get_model_xml_file_name());
+            Assert.IsTrue(model.Ptr != IntPtr.Zero);
+            PrePostProcessor processor = new PrePostProcessor(model);
+            InputInfo input = processor.input(0);
+            input.Dispose();
+            processor.Dispose();
+            model.Dispose();
+        }
+
+        [TestMethod()]
+        public void output_test()
+        {
+            var core = new Core();
+            Model model = core.read_model(get_model_xml_file_name());
+            Assert.IsTrue(model.Ptr != IntPtr.Zero);
+            PrePostProcessor processor = new PrePostProcessor(model);
+            OutputInfo input = processor.output();
+            input.Dispose();
+            processor.Dispose();
+            model.Dispose();
+        }
+
+        [TestMethod()]
+        public void output_test1()
+        {
+            var core = new Core();
+            Model model = core.read_model(get_model_xml_file_name());
+            Assert.IsTrue(model.Ptr != IntPtr.Zero);
+            PrePostProcessor processor = new PrePostProcessor(model);
+            OutputInfo input = processor.output(model_output_name());
+            input.Dispose();
+            processor.Dispose();
+            model.Dispose();
+        }
+
+        [TestMethod()]
+        public void output_test2()
+        {
+            var core = new Core();
+            Model model = core.read_model(get_model_xml_file_name());
+            Assert.IsTrue(model.Ptr != IntPtr.Zero);
+            PrePostProcessor processor = new PrePostProcessor(model);
+            OutputInfo input = processor.output(0);
+            input.Dispose();
+            processor.Dispose();
+            model.Dispose();
+        }
+
+        [TestMethod()]
+        public void build_test()
+        {
+            var core = new Core();
+            Model model = core.read_model(get_model_xml_file_name());
+            Assert.IsTrue(model.Ptr != IntPtr.Zero);
+            PrePostProcessor processor = new PrePostProcessor(model);
+
+            Tensor input_tensor = new Tensor(new OvType(ElementType.U8), new Shape(1, 640, 640, 3));
+            InputInfo input_info = processor.input(0);
+            InputTensorInfo input_tensor_info = input_info.tensor();
+            input_tensor_info.set_from(input_tensor).set_layout(new Layout("NHWC")).set_color_format(ColorFormat.BGR);
+
+            PreProcessSteps process_steps = input_info.preprocess();
+            process_steps.convert_color(ColorFormat.RGB).resize(ResizeAlgorithm.RESIZE_LINEAR)
+                .convert_element_type(new OvType(ElementType.F32)).scale(255.0f).convert_layout(new Layout("NCHW"));
+
+            Model new_model = processor.build();
+            new_model.Dispose();
+            processor.Dispose();
+            model.Dispose();
+        }
+    }
+}
