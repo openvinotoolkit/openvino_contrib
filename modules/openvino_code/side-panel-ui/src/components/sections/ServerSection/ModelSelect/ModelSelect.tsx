be15bef5cd5aa16806b1043b229a49f07b00284b
kumarijy <jyoti.kumari@intel.com>
2024-01-24 22:28:33 -0800

    Adding FIM support through deepseek-coder-1.3b-instruct and changed model names at FE

--- modules/openvino_code/side-panel-ui/src/components/sections/ServerSection/ModelSelect/ModelSelect.tsx
+++ modules/openvino_code/side-panel-ui/src/components/sections/ServerSection/ModelSelect/ModelSelect.tsx
@@ -7,6 +7,7 @@ const options: SelectOptionProps<ModelName>[] = [
   { value: ModelName.CODE_T5_220M },
   { value: ModelName.DECICODER_1B_OPENVINO_INT8 },
   { value: ModelName.STABLECODE_COMPLETION_ALPHA_3B_4K_OPENVINO_INT8 },
+  { value: ModelName.deepseek_coder_7b },
 ];
 
 interface ModelSelectProps {
