e15058d23277c8892bb07805a16aacc87a4879a4
kumarijy <112030960+kumarijy@users.noreply.github.com>
2024-03-19 02:47:10 -0700

    adding phi-2 model (#893)

--- modules/openvino_code/side-panel-ui/src/components/sections/ServerSection/ModelSelect/ModelSelect.tsx
+++ modules/openvino_code/side-panel-ui/src/components/sections/ServerSection/ModelSelect/ModelSelect.tsx
@@ -8,6 +8,7 @@ const options: SelectOptionProps<ModelName>[] = [
   { value: ModelName.DECICODER_1B_OPENVINO_INT8 },
   { value: ModelName.STABLECODE_COMPLETION_ALPHA_3B_4K_OPENVINO_INT8 },
   { value: ModelName.DEEPSEEK_CODER_1_3B },
+  { value: ModelName.PHI_2_2_7B },
 ];
 
 interface ModelSelectProps {
