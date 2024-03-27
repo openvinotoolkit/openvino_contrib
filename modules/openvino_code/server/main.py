4a59b724411c9929d8fc2da189cfdb9f5092b8d0
kumarijy <jyoti.kumari@intel.com>
2024-03-26 21:14:38 -0700

    update main.py

--- modules/openvino_code/server/main.py
+++ modules/openvino_code/server/main.py
@@ -3,10 +3,10 @@ from src.utils import setup_logger, get_parser
 # Logger should be set up before other imports to propagate logging config to other packages
 setup_logger()
 
-from src.generators import get_generator_dependency  # noqa: E402
-from src.app import app, get_generator_dummy  # noqa: E402
-
 import uvicorn  # noqa: E402
+
+from src.app import app, get_generator_dummy  # noqa: E402
+from src.generators import get_generator_dependency  # noqa: E402
 
 
 def main():
