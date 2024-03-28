859b6b4cfd10f9b54f846be5972b34f63e45a78a
kumarijy <jyoti.kumari@intel.com>
2024-03-27 21:57:43 -0700

    update main py

--- modules/openvino_code/server/main.py
+++ modules/openvino_code/server/main.py
@@ -1,4 +1,5 @@
-from src.utils import setup_logger, get_parser # noqa: I001
+from src.utils import get_parser, setup_logger
+
 
 # Logger should be set up before other imports to propagate logging config to other packages
 setup_logger()
@@ -11,6 +12,7 @@ from src.generators import get_generator_dependency  # noqa: E402
 
 def main():
     args = get_parser().parse_args()
+
     # temporary solution for cli args passing
     generator_dependency = get_generator_dependency(args.model, args.device, args.tokenizer_checkpoint, args.assistant)
     app.dependency_overrides[get_generator_dummy] = generator_dependency
