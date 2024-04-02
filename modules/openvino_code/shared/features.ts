8e987c412d841144f42513e6e39463bca8f048f8
kumarijy <jyoti.kumari@intel.com>
2024-04-01 23:42:09 -0700

    add more features to phi-2 model

--- modules/openvino_code/shared/features.ts
+++ modules/openvino_code/shared/features.ts
@@ -2,4 +2,6 @@ export enum Features {
   CODE_COMPLETION = 'Code Completion',
   SUMMARIZATION = 'Summarization',
   FIM = 'Fill-in-the-middle',
+  QA_FORMAT = 'QA Format',
+  CHAT_FORMAT = 'Chat Format',
 }
