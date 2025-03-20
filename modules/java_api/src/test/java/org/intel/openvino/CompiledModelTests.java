14bfecc3a0253ccb1c28c7cfed5ad56a563fdd4e
Raasz, Pawel <pawel.raasz@intel.com>
2025-03-20 15:38:19 +0000

    Update java test
    Signed-off-by: Raasz, Pawel <pawel.raasz@intel.com>

--- modules/java_api/src/test/java/org/intel/openvino/CompiledModelTests.java
+++ modules/java_api/src/test/java/org/intel/openvino/CompiledModelTests.java
@@ -34,7 +34,7 @@ public class CompiledModelTests extends OVTest {
         List<Output> outputs = model.outputs();
 
         assertEquals("fc_out", outputs.get(0).get_any_name());
-        assertEquals(ElementType.f32, outputs.get(0).get_element_type());
+        assertEquals(ElementType.f16, outputs.get(0).get_element_type());
 
         int[] shape = new int[] {1, 10};
         assertArrayEquals("Shape", shape, outputs.get(0).get_shape());
