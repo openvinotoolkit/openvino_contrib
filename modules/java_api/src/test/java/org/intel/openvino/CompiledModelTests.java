ef1729059d4cd2ccf750c70c2a7205974f61b23d
Pawel Raasz <pawel.raasz@intel.com>
2025-03-25 08:56:35 +0100

    Update tensor pointer cast (#952)
    * Update tensor pointer cast
    
    Signed-off-by: Raasz, Pawel <pawel.raasz@intel.com>
    
    * Experiment with action cache
    
    Signed-off-by: Raasz, Pawel <pawel.raasz@intel.com>
    
    * Update actions cache for mac and windows
    
    Signed-off-by: Raasz, Pawel <pawel.raasz@intel.com>
    
    * Update java test
    
    Signed-off-by: Raasz, Pawel <pawel.raasz@intel.com>
    
    * Update java tests
    
    Signed-off-by: Raasz, Pawel <pawel.raasz@intel.com>
    
    * Update code style
    
    Signed-off-by: Raasz, Pawel <pawel.raasz@intel.com>
    
    * Update code_style flow
    
    Signed-off-by: Raasz, Pawel <pawel.raasz@intel.com>
    
    * Restore code style flow
    
    Signed-off-by: Raasz, Pawel <pawel.raasz@intel.com>
    
    * Add hashes to GH actions
    
    Signed-off-by: Raasz, Pawel <pawel.raasz@intel.com>
    
    ---------
    
    Signed-off-by: Raasz, Pawel <pawel.raasz@intel.com>

--- modules/java_api/src/test/java/org/intel/openvino/CompiledModelTests.java
+++ modules/java_api/src/test/java/org/intel/openvino/CompiledModelTests.java
@@ -23,7 +23,7 @@ public class CompiledModelTests extends OVTest {
         List<Output> inputs = model.inputs();
 
         assertEquals("data", inputs.get(0).get_any_name());
-        assertEquals(ElementType.f32, inputs.get(0).get_element_type());
+        assertEquals(ElementType.f16, inputs.get(0).get_element_type());
 
         int[] shape = new int[] {1, 3, 32, 32};
         assertArrayEquals("Shape", shape, inputs.get(0).get_shape());
@@ -34,7 +34,7 @@ public class CompiledModelTests extends OVTest {
         List<Output> outputs = model.outputs();
 
         assertEquals("fc_out", outputs.get(0).get_any_name());
-        assertEquals(ElementType.f32, outputs.get(0).get_element_type());
+        assertEquals(ElementType.f16, outputs.get(0).get_element_type());
 
         int[] shape = new int[] {1, 10};
         assertArrayEquals("Shape", shape, outputs.get(0).get_shape());
