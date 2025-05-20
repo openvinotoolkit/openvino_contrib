b95f7fe3d6aba4e357b39d43e72ebe863685af55
Garth Long <garth.long@intel.com>
2025-05-20 15:20:33 -0700

    OpenVINO Code Completion is no longer maintained.

--- modules/openvino_code/src/inline-completion/tab.ts
+++
@@ -1,9 +0,0 @@
-let isGeneralTabActiveInternal: boolean = false;
-
-export function setIsGeneralTabActive(value: boolean): void {
-  isGeneralTabActiveInternal = value;
-}
-
-export function getIsGeneralTabActive(): boolean {
-  return isGeneralTabActiveInternal;
-}
