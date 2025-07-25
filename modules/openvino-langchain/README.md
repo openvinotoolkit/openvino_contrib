172388147c76095a59a0fbf7b87193aafb1557de
Kirill Suvorov <kirill.suvorov@intel.com>
2025-07-25 08:55:38 -0700

    [openvino-langchain] Add ChatOpenVINO (#983)
    Signed-off-by: Kirill Suvorov <kirill.suvorov@intel.com>
    Co-authored-by: Alicja Miloszewska <alicja.miloszewska@intel.com>

--- modules/openvino-langchain/README.md
+++ modules/openvino-langchain/README.md
@@ -64,9 +64,9 @@ optimum-cli export openvino --model "TinyLlama/TinyLlama-1.1B-Chat-v1.0" --weigh
 
 ## LLM
 
-This package contains the `GenAI` class, which is the recommended way to interact with models optimized for the OpenVINO toolkit.
+This package contains the `OpenVINO` class, which is the recommended way to interact with models optimized for the OpenVINO toolkit.
 
-**GenAI Parameters**
+**OpenVINO Parameters**
 
 | Name  | Type | Required | Description |
 | ----- | ---- |--------- | ----------- |
@@ -75,9 +75,9 @@ This package contains the `GenAI` class, which is the recommended way to interac
 | generationConfig | [GenerationConfig](https://github.com/openvinotoolkit/openvino.genai/blob/master/src/js/lib/utils.ts#L107-L110) | ❌ | Structure to keep generation config parameters. |
 
 ```typescript
-import { GenAI } from "openvino-langchain";
+import { OpenVINO } from "openvino-langchain";
 
-const model = new GenAI({
+const model = new OpenVINO({
     modelPath: "path-to-model",
     device: "CPU",
     generationConfig: {
@@ -85,6 +85,38 @@ const model = new GenAI({
     },
   });
 const response = await model.invoke("Hello, world!");
+```
+
+## ChatModel
+
+This package contains the `ChatOpenVINO` class, which allow use the OpenVINO for chat pipelines.
+
+**ChatOpenVINO Parameters**
+
+| Name  | Type | Required | Description |
+| ----- | ---- |--------- | ----------- |
+| modelPath | string | ✅ | Path to the directory containing model xml/bin files and tokenizer |
+| device | string | ❌ | Device to run the model on (e.g., CPU, GPU). |
+| generationConfig | [GenerationConfig](https://github.com/openvinotoolkit/openvino.genai/blob/master/src/js/lib/utils.ts#L107-L110) | ❌ | Structure to keep generation config parameters. |
+
+```js
+import { ChatOpenVINO } from "openvino-langchain";
+import { HumanMessage, SystemMessage } from '@langchain/core/messages';
+
+const model = new ChatOpenVINO({
+    modelPath: "path-to-model",
+    device: "CPU",
+    generationConfig: {
+        "max_new_tokens": 100,
+    },
+  });
+
+const messages = [
+  new SystemMessage('Translate the following from English into German'),
+  new HumanMessage('Thank you!'),
+];
+const response = await model.invoke(messages);
+console.log(response.content);
 ```
 
 ## Text Embedding Model
---
+++ modules/openvino-langchain/src/tests/chat_models.int.test.ts
@@ -0,0 +1,223 @@
+import { test, expect, beforeAll } from '@jest/globals';
+import { ChatOpenVINO } from '../chat_models.js';
+import { BaseMessage, HumanMessage } from '@langchain/core/messages';
+import { GenerationConfig } from 'openvino-genai-node';
+import { describe } from 'node:test';
+import { z } from 'zod';
+
+const modelPath = process.env.MODEL_PATH;
+if (modelPath === undefined) throw new Error('MODEL_PATH doest not defined');
+
+describe('Test ChatOpenVINO', () => {
+  let modelWithBaseConfig: ChatOpenVINO;
+
+  const baseConfig: GenerationConfig = {
+    'max_new_tokens': 20,
+  };
+
+  beforeAll(() => {
+    modelWithBaseConfig = new ChatOpenVINO({
+      modelPath,
+      generationConfig: baseConfig,
+    });
+  });
+
+  /**
+    * invoke() tests
+    */
+  {
+    test('invoke()', async () => {
+      const res = await modelWithBaseConfig.invoke('what is 1 + 1?');
+      expect(res).toBeTruthy();
+      expect(res instanceof BaseMessage);
+      expect(res.content).toEqual(res.text);
+    });
+
+    test('invoke() with stop', async () => {
+      const res = await modelWithBaseConfig.invoke([
+        ['human', 'what is 1 + 1?'],
+      ], {
+        stop: ['2', 'two'],
+      });
+      expect(res).toBeTruthy();
+      expect(res.text.endsWith('2') || res.text.endsWith('two')).toBe(true);
+
+    });
+
+    test('invoke() with handleLLMNewToken callback', async () => {
+      const tokens: string[] = [];
+      const res = await modelWithBaseConfig.invoke(
+        [new HumanMessage('what is 1 + 1?')],
+        {
+          callbacks: [
+            {
+              handleLLMNewToken(token) {
+                tokens.push(token);
+              },
+            },
+          ],
+        },
+      );
+      expect(tokens.length).toBeGreaterThanOrEqual(1);
+      expect(tokens.join('')).toBe(res.text);
+    });
+
+    test('invoke() is interrupted by the abort signal', async () => {
+      await expect(() => modelWithBaseConfig.invoke(
+        'what is 1 + 1?',
+        {
+          signal: AbortSignal.timeout(1),
+        },
+      ),
+      ).rejects.toThrow();
+    });
+
+    test('invoke() is interrupted by timeout', async () => {
+      await expect(() => modelWithBaseConfig.invoke(
+        'what is 1 + 1?',
+        {
+          timeout: 1,
+        },
+      ),
+      ).rejects.toThrow();
+    });
+
+  }
+
+  /**
+    * stream() tests
+    */
+  {
+    test('stream()', async () => {
+      let res = '';
+      const streamer = await modelWithBaseConfig.stream('what is 1 + 1?');
+      for await (const streamItem of streamer) {
+        res += streamItem.text;
+        expect(streamItem instanceof BaseMessage);
+        expect(streamItem.content).toEqual(streamItem.text);
+      }
+      expect(res).toBeTruthy();
+    });
+
+    test.skip('stream() with stop', async () => {
+      let chunks: string[] = [];
+      const streamer = await modelWithBaseConfig.stream(
+        'what is 1 + 1?',
+        {
+          stop: ['2', 'two'],
+        },
+      );
+      for await (const streamItem of streamer) {
+        chunks.push(streamItem.text);
+      }
+      expect(chunks.length).toBeGreaterThan(0);
+      expect(chunks[chunks.length - 1].includes('2') || chunks[chunks.length - 1].includes('two')).toBe(true);
+    });
+
+    test('stream() with handleLLMNewToken callback', async () => {
+      const tokens: string[] = [];
+      let responseContent = '';
+      const streamer = await modelWithBaseConfig.stream(
+        [new HumanMessage('what is 1 + 1?')],
+        {
+          callbacks: [
+            {
+              handleLLMNewToken(token) {
+                tokens.push(token);
+              },
+            },
+          ],
+        },
+      );
+      for await (const streamItem of streamer) {
+        responseContent += streamItem.text;
+      }
+      expect(tokens.length).toBeGreaterThanOrEqual(1);
+      expect(tokens.join('')).toBe(responseContent);
+    });
+
+    // stream doesn't support a signal yet
+    test.skip('stream() is interrupted by the abort signal', async () => {
+      const streamer = await modelWithBaseConfig.stream(
+        [new HumanMessage('what is 1 + 1?')],
+        {
+          signal: AbortSignal.timeout(1),
+        },
+      );
+      expect(async () => {
+        const tokens: string[] = [];
+        for await (const streamItem of streamer) {
+          tokens.push(streamItem.text);
+        }
+      }).rejects.toThrow();
+    });
+
+    // stream doesn't support a timeout yet
+    test.skip('stream() is interrupted by timeout', async () => {
+      const streamer = await modelWithBaseConfig.stream(
+        [new HumanMessage('what is 1 + 1?')],
+        {
+          timeout: 1,
+        },
+      );
+      expect(async () => {
+        const tokens: string[] = [];
+        for await (const streamItem of streamer) {
+          tokens.push(streamItem.text);
+        }
+      }).rejects.toThrow();
+    });
+
+    // It will be fixed in the next release of openvino-genai-node
+    test.skip('stream() is interrupted by early break', async () => {
+      const tokens: string[] = [];
+      const streamer = await modelWithBaseConfig.stream(
+        [new HumanMessage('what is 1 + 1?')],
+      );
+      for await (const streamItem of streamer) {
+        tokens.push(streamItem.text);
+        if (tokens.length >= 5) {
+          break;
+        }
+      }
+      expect(tokens.length).toEqual(5);
+    });
+  }
+
+  /**
+    * other API
+    */
+  {
+    test('Using a system message', async () => {
+      const res = await modelWithBaseConfig.invoke([
+        ['system', 'You are an amazing translator.'],
+        ['human', 'Translate "I love programming" into Korean.'],
+      ]);
+      expect(res).toBeTruthy();
+      expect(res instanceof BaseMessage);
+    });
+
+    test('generate()', async () => {
+      const res = await modelWithBaseConfig.generate([
+        ['human', 'what is 1 + 1?'],
+      ]);
+      expect(res).toBeTruthy();
+    });
+
+    // ChatOpenVINO doesn't support a structure output yet
+    test.skip('ChatOpenVINO with withStructuredOutput', async () => {
+      const modelWithTools = modelWithBaseConfig.withStructuredOutput(
+        z.object({
+          zomg: z.string(),
+          omg: z.number().optional(),
+        }),
+      );
+      const prompt = new HumanMessage(
+        'Search the web and tell me what the weather '
+        + 'will be like tonight in new york. use weather.com',
+      );
+      const res = await modelWithTools.invoke([prompt]);
+      expect(typeof res.zomg === 'string').toBe(true);
+    });
+  }
+});
