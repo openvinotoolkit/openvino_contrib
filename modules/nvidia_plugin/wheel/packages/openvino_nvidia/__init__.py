3f4eedaabdaaf54cf6e931b5bf7f800fb181a63a
Denis <redradist@gmail.com>
2025-09-24 10:28:05 +0300

    [NVIDIA] Prepare setup.py for PyPi (#1001)
    * Fix issues related to auditwheel and final .whl package
    
    * Refactored setup.py, to make consistent case where OPENVINO_SRC_DIR was provided and openvino downloaded from the internet
    
    * Updated openvino_nvidia/__init__.py to improve registration of NVIDIA plugin; update README.,d by adding the section related to how build python package for NVIDIA plugin
    
    ---------
    
    Co-authored-by: Denis Kotov <denys.kotov@altran.com>

--- modules/nvidia_plugin/wheel/packages/openvino_nvidia/__init__.py
+++ modules/nvidia_plugin/wheel/packages/openvino_nvidia/__init__.py
@@ -18,22 +18,44 @@ def _get_lib_file_extension() -> str:
         return "dylib"
 
 
-def _register_nvidia_plugin():
+def _register_nvidia_plugin(force=False):
     import openvino
     openvino_package_dir = os.path.dirname(os.path.abspath(openvino.__file__))
     openvino_package_libs_dir = os.path.join(openvino_package_dir, "libs")
     openvino_nvidia_gpu_package_dir = os.path.dirname(os.path.abspath(__file__))
-    openvino_nvidia_gpu_library = os.path.join(openvino_nvidia_gpu_package_dir, f"../libopenvino_nvidia_gpu_plugin.{_get_lib_file_extension()}")
+    openvino_nvidia_gpu_library = os.path.join(openvino_nvidia_gpu_package_dir, f"./lib/libopenvino_nvidia_gpu_plugin.{_get_lib_file_extension()}")
 
     xml_file = os.path.join(openvino_package_libs_dir, "plugins.xml")
+    xml_file_updated = False
     tree = ET.parse(xml_file).getroot()
     plugins = tree.find("plugins")
-    if all(plugin.get('name') != 'NVIDIA' for plugin in plugins.iter('plugin')):
-        plugins.append(Element('plugin', {'name': 'NVIDIA', 'location': openvino_nvidia_gpu_library}))
+    
+    if force:
+        for plugin in plugins:
+            if plugin.tag == "plugin" and plugin.get("name") == "NVIDIA":
+                plugins.remove(plugin)
+                plugins.append(Element('plugin', {'name': 'NVIDIA', 'location': openvino_nvidia_gpu_library}))
+                xml_file_updated = True
+    else:
+        if all(plugin.get('name') != 'NVIDIA' for plugin in plugins.iter('plugin')):
+            plugins.append(Element('plugin', {'name': 'NVIDIA', 'location': openvino_nvidia_gpu_library}))
+            xml_file_updated = True
+    
+    if xml_file_updated:
         with open(xml_file, "w") as f:
             f.write(ET.tostring(tree).decode('utf8'))
 
 
+def __getattr__(name):
+    if name == "install":
+        _register_nvidia_plugin()
+    elif name == "force_install":
+        _register_nvidia_plugin(True)
+    else:
+        raise AttributeError(f"module {__name__} has no attribute {name}")
+
+
 _register_nvidia_plugin()
 
-__version__ = "2024.1.0"
+
+__version__ = "2025.3.0"
