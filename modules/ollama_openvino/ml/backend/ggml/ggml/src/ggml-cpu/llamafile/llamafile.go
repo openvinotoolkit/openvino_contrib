ff377f8693ba22c5d2bd6b86b25be460d82c64c2
Vaibhav-Upadhyaya <vaibhav.21928@stu.upes.ac.in>
2026-03-23 12:47:52 +0530

    Add NPU fallback warning (Resolves #996)

--- modules/ollama_openvino/ml/backend/ggml/ggml/src/ggml-cpu/llamafile/llamafile.go
+++ modules/ollama_openvino/ml/backend/ggml/ggml/src/ggml-cpu/llamafile/llamafile.go
@@ -1,5 +1,6 @@
 package llamafile
 
-// #cgo CXXFLAGS: -std=c++17
+// #cgo !windows CXXFLAGS: -std=c++17
+// #cgo windows CXXFLAGS: -std=c++17
 // #cgo CPPFLAGS: -I${SRCDIR}/.. -I${SRCDIR}/../.. -I${SRCDIR}/../../../include
 import "C"
