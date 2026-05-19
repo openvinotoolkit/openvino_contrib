2eb3d0af8e4d48023bfcacd141f8617add7df31a
Rohit Verma <61152664+rv355@users.noreply.github.com>
2026-05-19 21:14:41 +0530

    Module to enable BEVFusion with OpenVino (#1056)
    * added bevfusion module
    
    * Updated README
    
    * added Intel copyrights and torchvision license

---
+++ modules/openvino_bevfusion/openvino_extensions/voxelize/ov_extension.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2018-2026 Intel Corporation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// OpenVINO Extension Entry Point for Voxelization Operations
+
+#include <openvino/core/extension.hpp>
+#include <openvino/core/op_extension.hpp>
+#include <openvino/frontend/extension.hpp>
+
+#include "voxelize_op.hpp"
+
+OPENVINO_CREATE_EXTENSIONS(
+    std::vector<ov::Extension::Ptr>({
+        std::make_shared<ov::OpExtension<BEVFusionExtension::VoxelizeScatter>>(),
+        std::make_shared<ov::frontend::OpExtension<BEVFusionExtension::VoxelizeScatter>>(),
+        std::make_shared<ov::OpExtension<BEVFusionExtension::VoxelizeMean>>(),
+        std::make_shared<ov::frontend::OpExtension<BEVFusionExtension::VoxelizeMean>>(),
+    }));
