273a90bce621d6832dccbe3fe5b0c6f5bced8402
Mohammad Haghighipanah <mohammad.haghighipanah@intel.com>
2026-07-08 09:24:06 -0700

    updated copyright info
    Signed-off-by: Mohammad Haghighipanah <mohammad.haghighipanah@intel.com>

--- modules/openvino_flashocc/flashocc/__init__.py
+++ modules/openvino_flashocc/flashocc/__init__.py
@@ -1,6 +1,5 @@
 # Copyright (C) 2018-2026 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
-# Reimplementation of FlashOCC under Apache-2.0-compatible terms.
 
 from .builder import build_flashocc_model
 from .config import load_config
