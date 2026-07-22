306fae7fee649dfba0a1542856936ab6febc796e
Mohammad Haghighipanah <mohammad.haghighipanah@intel.com>
2026-07-22 12:18:28 -0700

    updated readme
    Signed-off-by: Mohammad Haghighipanah <mohammad.haghighipanah@intel.com>

--- modules/openvino_flashocc/setup.sh
+++ modules/openvino_flashocc/setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Copyright (C) 2018-2026 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
 # =============================================================================
 # setup.sh — FlashOCC OpenVINO 2026.3 environment setup
 #
